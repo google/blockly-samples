a485c63707e8a3eb08a12639edffd87481771916
Sam El-Husseini <sam.elhusseini@gmail.com>
2019-12-04 17:08:11 -0800

    Add a React Date field that uses the react-datepicker package.

--- blockly-react/src/blocks/customblocks.js
+++ blockly-react/src/blocks/customblocks.js
@@ -29,6 +29,7 @@ import * as Blockly from 'blockly/core';
 
 // Since we're using json to initialize the field, we'll need to import it.
 import '../fields/BlocklyReactField';
+import '../fields/DateField';
 
 var testReactField = {
   "type": "test_react_field",
@@ -47,6 +48,27 @@ var testReactField = {
 Blockly.Blocks['test_react_field'] = {
   init: function() {
     this.jsonInit(testReactField);
+    this.setStyle('loop_blocks');
+  }
+};
+
+var reactDateField = {
+  "type": "test_react_date_field",
+  "message0": "date field %1",
+  "args0": [
+    {
+      "type": "field_react_date",
+      "name": "DATE",
+      "date": "01/01/2020"
+    },
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+};
+
+Blockly.Blocks['test_react_date_field'] = {
+  init: function() {
+    this.jsonInit(reactDateField);
     this.setStyle('loop_blocks');
   }
 };
