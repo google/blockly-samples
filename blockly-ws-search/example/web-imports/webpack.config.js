471326f4afd1509dda6268bc9cc76884163dc524
Monica Kozbial <kozbial@google.com>
2020-02-28 13:22:12 -0800

    Creating web-imports example. (#92)

---
+++ blockly-ws-search/example/web-imports/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+  }
+};
