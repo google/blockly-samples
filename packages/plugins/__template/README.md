2c9d90ae47113f4d67667208e8a90b6fa41f3060
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-04-03 14:52:48 -0700

    Add plugin template

---
+++ packages/plugins/__template/README.md
@@ -0,0 +1,32 @@
+# @blockly/plugin-template [![Built on Blockly](https://tinyurl.com/built-on-blockly)](https://github.com/google/blockly)
+
+A [Blockly](https://www.npmjs.com/package/blockly) plugin that ...
+
+## Installation
+
+### Yarn
+```
+yarn add @blockly/plugin-template
+```
+
+### npm
+```
+npm install @blockly/plugin-template --save
+```
+
+## Usage
+
+```js
+import Blockly from 'blockly';
+import { ... } from '@blockly/plugin-template';
+
+// TODO, initialize, usage.
+
+```
+
+## API
+
+TODO
+
+## License
+Apache 2.0
---
+++ packages/plugins/__template/src/index.js
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview TODO: Add plugin overview.
+ */
+
+/**
+ * TODO: Rename plugin and add plugin description.
+ */
+export default class Plugin {
+  /**
+   * Constructor for ...
+   * @param {!Blockly.WorkspaceSvg} workspace The workspace that the plugin will
+   *     be added to.
+   */
+  constructor(workspace) {
+    /**
+     * The workspace.
+     * @type {!Blockly.WorkspaceSvg}
+     * @protected
+     */
+    this.workspace_ = workspace;
+  }
+
+  /**
+   * Initialize.
+   */
+  init() { }
+}
