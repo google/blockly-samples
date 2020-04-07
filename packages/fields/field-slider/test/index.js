aa3ab7f368006765e8fac90616bca868c39cfc14
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-04-07 14:57:42 -0700

    Lint field slider (#185)

--- packages/fields/field-slider/test/index.js
+++ packages/fields/field-slider/test/index.js
@@ -14,28 +14,28 @@ import '../src/index.js';
 
 Blockly.defineBlocksWithJsonArray([
   {
-    "type": "test_field_slider",
-    "message0": "slider: %1",
-    "args0": [
+    'type': 'test_field_slider',
+    'message0': 'slider: %1',
+    'args0': [
       {
-        "type": "field_slider",
-        "name": "FIELDNAME",
-        "value": 50,
-        "alt":
+        'type': 'field_slider',
+        'name': 'FIELDNAME',
+        'value': 50,
+        'alt':
             {
-              "type": "field_label",
-              "text": "NO_SLIDER_FIELD"
-            }
-      }
+              'type': 'field_label',
+              'text': 'NO_SLIDER_FIELD',
+            },
+      },
     ],
-    "style": "math_blocks",
+    'style': 'math_blocks',
   }]);
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
   Blockly.inject('blocklyDiv',
       {
-        toolbox: `<xml xmlns="https://developers.google.com/blockly/xml" style="display: none">
+        toolbox: `<xml xmlns="https://developers.google.com/blockly/xml">
           <block type="test_field_slider"></block>
-        </xml>`
+        </xml>`,
       });
 });
