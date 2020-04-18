a1d7aa846ad87a4678049f9844b1a6a1e2702ffb
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-04-17 21:31:09 -0700

    Add the theme template and update create script (#216)
    * Add the theme template

---
+++ plugins/dev-create/templates/theme/template/src/index.js
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// TODO: Edit theme overview.
+/**
+ * @fileoverview Theme overview.
+ */
+
+import Blockly from 'blockly/core';
+
+// TODO: Edit theme name.
+export default Blockly.Theme.defineTheme('THEME_NAME', {
+  'base': Blockly.Themes.Classic,
+  'blockStyles': {},
+  'categoryStyles': {},
+  'componentStyles': {},
+  'fontStyle': {},
+  'startHats': null,
+});
