63b20e5e08718b14eb8e94d082d77ea03e4236f2
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-05-05 13:24:51 -0700

    Add field date tests (#251)
    * Add field date tests

---
+++ plugins/field-date/externs.js
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for Field date.
+ * @externs
+ */
+
+goog.provide('Blockly');
+goog.provide('Blockly.Css');
+goog.provide('Blockly.Events');
+goog.provide('Blockly.Field');
+goog.provide('Blockly.fieldRegistry');
+goog.provide('Blockly.utils.dom');
+goog.provide('Blockly.utils.object');
+goog.provide('Blockly.utils.string');
+
+var Blockly;
--- plugins/field-date/gulpfile.js
+++ plugins/field-date/gulpfile.js
@@ -9,54 +9,49 @@
  * @author samelh@google.com (Sam El-Husseini)
  */
 
-var gulp = require('gulp');
+const gulp = require('gulp');
 gulp.replace = require('gulp-replace');
 gulp.umd = require('gulp-umd');
 
-var closureCompiler = require('google-closure-compiler').gulp();
+const closureCompiler = require('google-closure-compiler').gulp();
 
+/**
+ * Build script.
+ * @return {Function} The gulp task.
+ */
 function build() {
-  const provides = `
-goog.provide('Blockly');
-goog.provide('Blockly.Css');
-goog.provide('Blockly.Events');
-goog.provide('Blockly.Field');
-goog.provide('Blockly.fieldRegistry');
-goog.provide('Blockly.utils.dom');
-goog.provide('Blockly.utils.object');
-goog.provide('Blockly.utils.string');`;
   return gulp.src([
     'src/**',
-    './node_modules/google-closure-library/closure/goog/**/**/*.js'
-  ], { base: './' })
-    // Add Blockly provides to be compatible with the compiler.
-    .pipe(gulp.replace(`goog.provide('Blockly.FieldDate');`,
-      `${provides}goog.provide('Blockly.FieldDate');`))
-    .pipe(closureCompiler({
-      dependency_mode: 'PRUNE',
-      entry_point: './src/field_date.js',
-      js_output_file: 'date_compressed.js',
-      language_in: 'ECMASCRIPT_2015'
-    }))
-    // Remove the Blockly provides to be compatible with Blockly.
-    .pipe(gulp.replace(/var Blockly=\{[^;]*\};\n?/, ''))
-    .pipe(gulp.replace(/Blockly\.utils[^=\(]+=\{[^;]*\};/g, ''))
-    .pipe(gulp.umd({
-      dependencies: function () {
-        return [{
-          name: 'Blockly',
-          amd: 'blockly/core',
-          cjs: 'blockly/core',
-          global: 'Blockly'
-        }];
-      },
-      namespace: function () { return 'Blockly.FieldDate'; },
-      exports: function () { return 'Blockly.FieldDate'; },
-    }))
-    .pipe(gulp.dest('./dist/'));
-};
+    './node_modules/google-closure-library/closure/goog/**/**/*.js',
+  ], {base: './'})
+      .pipe(closureCompiler({
+        dependency_mode: 'PRUNE',
+        entry_point: './src/field_date.js',
+        js_output_file: 'date_compressed.js',
+        language_in: 'ECMASCRIPT_2015',
+        externs: ['./externs.js'],
+      }))
+  // Remove the Blockly provides to be compatible with Blockly.
+      .pipe(gulp.umd({
+        dependencies: function() {
+          return [{
+            name: 'Blockly',
+            amd: 'blockly/core',
+            cjs: 'blockly/core',
+            global: 'Blockly',
+          }];
+        },
+        namespace: function() {
+          return 'Blockly.FieldDate';
+        },
+        exports: function() {
+          return 'Blockly.FieldDate';
+        },
+      }))
+      .pipe(gulp.dest('./dist/'));
+}
 
 module.exports = {
   default: build,
-  build
+  build,
 };
