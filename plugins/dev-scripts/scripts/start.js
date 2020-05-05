a96f1488469ba0ed4febb61bb7e370acfdc2878f
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-05-05 09:17:11 -0700

    Less noisy emit of webpack, eslint and ts messages

--- plugins/dev-scripts/scripts/start.js
+++ plugins/dev-scripts/scripts/start.js
@@ -18,16 +18,27 @@
 
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('../config/webpack.config');
 const webpackDevServerConfig = require('../config/webpackDevServer.config');
 
+const chalk = require('chalk');
+const codeFrame = require('@babel/code-frame').codeFrameColumns;
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
+const clearConsole = () => process.stdout.write(
+    process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H'
+);
+
 const packageJson = require(resolveApp('package.json'));
+const isTypescript = fs.existsSync(resolveApp('tsconfig.json'));
+
 console.log(`Running start for ${packageJson.name}`);
 
 // Create the webpack configuration for the development environment.
@@ -35,7 +46,132 @@ console.log(`Running start for ${packageJson.name}`);
 const config = webpackConfig({
   mode: 'development',
 });
-const compiler = webpack(config);
+let compiler;
+try {
+  compiler = webpack(config);
+} catch (err) {
+  console.log(`${chalk.red('Failed to compile.')}\n${err.message || err}\n`);
+  process.exit(1);
+}
+
+const devSocket = {
+  warnings: (warnings) =>
+    devServer.sockWrite(devServer.sockets, 'warnings', warnings),
+  errors: (errors) =>
+    devServer.sockWrite(devServer.sockets, 'errors', errors),
+};
+
+compiler.hooks.invalid.tap('invalid', () => {
+  clearConsole();
+  console.log('Compiling...');
+});
+
+let tsMessagesPromise;
+let tsMessagesResolver;
+
+if (isTypescript) {
+  compiler.hooks.beforeCompile.tap('beforeCompile', () => {
+    tsMessagesPromise = new Promise((resolve) => {
+      tsMessagesResolver = (msgs) => resolve(msgs);
+    });
+  });
+
+  // Register TypeScript type checker hooks.
+  const tsCheckerHooks = ForkTsCheckerWebpackPlugin.getCompilerHooks(compiler);
+  tsCheckerHooks.receive.tap('done', (diagnostics, lints) => {
+    const allMsgs = [...diagnostics, ...lints];
+
+    const formatTypecheckerMessage = (message) => {
+      const {severity, file, line, character} = message;
+
+      const messageColor = severity == 'warning' ? chalk.yellow : chalk.red;
+
+      const source = file && fs.existsSync(file) &&
+        fs.readFileSync(file, 'utf-8');
+      const frame = source ? codeFrame(source,
+          {start: {line: line, column: character}},
+          {highlightCode: true, linesAbove: 2, linesBelow: 2})
+          .split('\n').map((str) => '  ' + str).join(os.EOL) : '';
+
+      return [
+        os.EOL,
+        messageColor.bold(`${severity.toLowerCase()} in `) +
+          chalk.cyan.bold(`${file}(${line},${character})`) +
+          messageColor(':'),
+        '',
+        frame,
+      ].join(os.EOL);
+    };
+
+    tsMessagesResolver({
+      errors: allMsgs
+          .filter((msg) => msg.severity === 'error')
+          .map(formatTypecheckerMessage),
+      warnings: allMsgs
+          .filter((msg) => msg.severity === 'warning')
+          .map(formatTypecheckerMessage),
+    });
+  });
+}
+
+// Register webpack compiler hooks.
+compiler.hooks.done.tap('done', async (stats) => {
+  clearConsole();
+
+  const statsData = stats.toJson({
+    all: false,
+    warnings: true,
+    errors: true,
+  });
+
+  if (isTypescript && statsData.errors.length === 0) {
+    const delayedMsg = setTimeout(() => {
+      console.log(chalk.yellow(
+          'Files successfully emitted, waiting for typecheck results...'));
+    }, 100);
+
+    const messages = await tsMessagesPromise;
+    clearTimeout(delayedMsg);
+
+    statsData.warnings.push(...messages.errors);
+    statsData.warnings.push(...messages.warnings);
+    stats.compilation.warnings.push(...messages.errors);
+    stats.compilation.warnings.push(...messages.warnings);
+
+    if (messages.errors.length > 0) {
+      devSocket.warnings(messages.errors);
+    } else if (messages.warnings.length > 0) {
+      devSocket.warnings(messages.warnings);
+    }
+
+    clearConsole();
+  }
+
+  const formatWebpackMessage = (message) => {
+    return message.trim();
+  };
+
+  const messages = {
+    errors: statsData.errors
+        .map(formatWebpackMessage),
+    warnings: statsData.warnings
+        .map(formatWebpackMessage),
+  };
+
+  // Emit compile output.
+  if (!messages.errors.length && !messages.warnings.length) {
+    console.log(chalk.green('Compiled successfully!'));
+  }
+  if (messages.errors.length) {
+    console.log(chalk.red('Failed to compile.\n'));
+    console.log(messages.errors.join('\n\n'));
+    return;
+  }
+  if (messages.warnings.length) {
+    console.log(chalk.yellow('Compiled with warnings.\n'));
+    console.log(messages.warnings.join('\n\n'));
+  }
+});
 
 // Read the webpack devServer configuration.
 const serverConfig = webpackDevServerConfig();
