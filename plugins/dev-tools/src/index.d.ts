36806a47a5ca4e0fa5bd32c82e43f1b5d718b0ec
Sam El-Husseini <sam.elhusseini@gmail.com>
2020-05-05 09:31:54 -0700

    Add jsdoc

--- plugins/dev-tools/src/index.d.ts
+++ plugins/dev-tools/src/index.d.ts
@@ -11,11 +11,21 @@ declare namespace DevTools {
     static init(): void;
   }
 
+  /**
+   * Use dat.GUI to add controls to adjust configuration of a Blockly workspace.
+   */
   function addGUIControls(createWorkspace:
       (options: Blockly.BlocklyOptions) => Blockly.Workspace,
       defaultOptions: Blockly.BlocklyOptions): dat.GUI;
 
+  /**
+   * A toolbox xml with built-in blocks split into categories.
+   */
   const toolboxCategories: string;
+
+  /**
+   * A simple toolbox xml with built-in blocks and no categories.
+   */
   const toolboxSimple: string;
 }
 
