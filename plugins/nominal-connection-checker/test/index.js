351a37ce18a35a73ae4775555ca4f5e0b1bec19f
Beka Westberg <bekawestberg@gmail.com>
2020-09-02 15:00:39 -0700

    Implement basic functionality

--- plugins/nominal-connection-checker/test/index.js
+++ plugins/nominal-connection-checker/test/index.js
@@ -9,16 +9,41 @@
  */
 
 import * as Blockly from 'blockly';
-import {toolboxCategories, createPlayground} from '@blockly/dev-tools';
+import {addCodeEditor} from '@blockly/dev-tools/src/playground/monaco';
+import {LocalStorageState} from '@blockly/dev-tools/src/playground/state';
+import {renderPlayground, renderCodeTab} from
+  '@blockly/dev-tools/src/playground/ui.js';
 import {Plugin} from '../src/index';
 
 /**
  * Create a workspace.
- * @param {HTMLElement} blocklyDiv The blockly container div.
- * @param {!Blockly.BlocklyOptions} options The Blockly options.
+ * @param {!HTMLElement} blocklyDiv
+ * @param {!Object} typeHierarchy
+ * @param {!Array<!Object>} blocks
  * @return {!Blockly.WorkspaceSvg} The created workspace.
  */
-function createWorkspace(blocklyDiv, options) {
+function createWorkspace(blocklyDiv, typeHierarchy, blocks) {
+  Blockly.defineBlocksWithJsonArray(blocks);
+  const toolbox = [];
+  blocks.forEach((def) => {
+    toolbox.push({
+      kind: 'BLOCK',
+      type: def.type,
+    });
+  });
+
+  const options = {
+    toolbox: toolbox,
+    move: {
+      scrollbars: true,
+      drag: true,
+      wheel: true,
+    },
+    zoom: {
+      wheel: true,
+    },
+  };
+
   const workspace = Blockly.inject(blocklyDiv, options);
 
   // TODO: Initialize your plugin here.
@@ -29,9 +54,153 @@ function createWorkspace(blocklyDiv, options) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  const defaultOptions = {
-    toolbox: toolboxCategories,
-  };
-  createPlayground(document.getElementById('root'), createWorkspace,
-      defaultOptions);
+  const container = document.getElementById('root');
+  const components = renderPlayground(container);
+  const blocklyDiv = components.blocklyDiv;
+  const monacoDiv = components.monacoDiv;
+  const tabsDiv = components.tabsDiv;
+  const guiContainer = components.guiContainer;
+
+  guiContainer.style.flex = '0';
+  monacoDiv.parentElement.style.maxHeight = '100%';
+
+  return addCodeEditor(monacoDiv, {
+    model: null,
+    language: 'xml',
+    minimap: {
+      enabled: false,
+    },
+    theme: 'vs-dark',
+    scrollBeyondLastLine: false,
+    automaticLayout: true,
+  }).then((editor) => {
+    function addCodeTab(name, language) {
+      const tabElement = renderCodeTab(name);
+      tabElement.setAttribute('data-tab', name);
+      tabsDiv.appendChild(tabElement);
+
+      const model = window.monaco.editor.createModel('', language);
+      editor.setModel(model);
+
+      const state = {
+        name,
+        model,
+        language,
+        viewState: undefined,
+      };
+
+      return {
+        state,
+        tabElement,
+      };
+    }
+
+    function setActiveTab(tab) {
+      currentTab = tab;
+      editor.setModel(currentTab.state.model);
+
+      // Update tab UI.
+      Object.values(tabs).forEach((t) =>
+        t.tabElement.style.background =
+              (t.tabElement == tab.tabElement) ? '#1E1E1E' : '#2D2D2D');
+      // Update editor state.
+      playgroundState.set('activeTab', tab.state.name);
+      playgroundState.save();
+    }
+
+    tabsDiv.addEventListener('click', (e) => {
+      const target = /** @type {HTMLElement} */ (e.target);
+      const tabName = target.getAttribute('data-tab');
+      if (!tabName) {
+        // Not a tab.
+        return;
+      }
+      const tab = tabs[tabName];
+
+      // Save current tab state (eg: scroll position).
+      currentTab.state.viewState = editor.saveViewState();
+
+      setActiveTab(tab);
+
+      // Restore tab state (eg: scroll position).
+      editor.restoreViewState(currentTab.state.viewState);
+      editor.focus();
+    });
+
+    function loadTab(name) {
+      const model = tabs[name].state.model;
+      model.setValue(playgroundState.get(name));
+      model.onDidChangeContent(() => {
+        playgroundState.set(name, model.getValue());
+        playgroundState.save();
+      });
+    }
+
+    function loadXml(workspace) {
+      let addListener = true;
+      try {
+        // Try to deserialize the saved xml.
+        Blockly.Xml.domToWorkspace(
+            Blockly.Xml.textToDom(playgroundState.get('workspaceXml')),
+            workspace);
+      } catch (e) {
+        // Xml load failed, ask if they want to clear the workspace.
+        const msg = `Xml load failed with error: ${e}
+            Clear workspace and continue?`;
+        if (confirm(msg)) {
+          workspace.clear();
+        } else {
+          addListener = false;
+        }
+      } finally {
+        // Add the change listener to save the workspace state.
+        if (addListener) {
+          workspace.addChangeListener(() => {
+            const xml = Blockly.Xml.domToPrettyText(
+                Blockly.Xml.workspaceToDom(workspace));
+            playgroundState.set('workspaceXml', xml);
+            playgroundState.save();
+          });
+        }
+      }
+    }
+
+    const typesTabName = 'Type Hierarchy';
+    const blocksTabName = 'Blocks';
+
+    const tabs = {
+      [typesTabName]: addCodeTab(typesTabName, 'json'),
+      [blocksTabName]: addCodeTab(blocksTabName, 'json'),
+    };
+
+    const playgroundState = new LocalStorageState('playgroundState', {
+      workspaceXml: '',
+      activeTab: typesTabName,
+      [typesTabName]: '{\n    \n}',
+      [blocksTabName]:
+          '[\n' +
+          '    {\n' +
+          '        "type": "my_block_type"\n' +
+          '    }\n' +
+          ']',
+    });
+    playgroundState.load();
+
+    loadTab(typesTabName);
+    loadTab(blocksTabName);
+
+    let currentTab = tabs[playgroundState.get('activeTab')];
+    setActiveTab(currentTab);
+
+    try {
+      // Create the workspace.
+      const workspace = createWorkspace(blocklyDiv,
+          JSON.parse(tabs[typesTabName].state.model.getValue()),
+          JSON.parse(tabs[blocksTabName].state.model.getValue()));
+      loadXml(workspace);
+    } catch (e) {
+      // JSON parse failed, send an alert.
+      alert(e);
+    }
+  });
 });
