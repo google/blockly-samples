4bd8be7be6fa27d9d1a3c36c8e472dbb03e9a514
Maribeth Bottorff <maribethb@google.com>
2020-11-10 14:03:55 -0800

    Add a checkbox for enabling arrow key navigation

--- examples/keyboard-accessibility/test/index.js
+++ examples/keyboard-accessibility/test/index.js
@@ -8,6 +8,7 @@
  * @fileoverview Plugin test.
  */
 
+import Blockly from 'blockly/core';
 import {speaker} from '../src/speaker';
 import {notePlayer} from '../src/note_player';
 import {Music} from '../src/music';
@@ -34,6 +35,25 @@ document.addEventListener('DOMContentLoaded', function() {
           notePlayer.playNote('C4', '8n');
         });
       });
+  document.getElementById('setLevel').addEventListener(
+      'input', function(event) {
+        game.loadLevel(this.value);
+      });
+  document.getElementById('enableArrowKeys').addEventListener('click',
+      (event) => {
+        const keyCodeMappings = [
+          [Blockly.utils.KeyCodes.UP, Blockly.navigation.actionNames.PREVIOUS],
+          [Blockly.utils.KeyCodes.DOWN, Blockly.navigation.actionNames.NEXT],
+          [Blockly.utils.KeyCodes.RIGHT, Blockly.navigation.actionNames.IN],
+          [Blockly.utils.KeyCodes.LEFT, Blockly.navigation.actionNames.OUT]];
+        keyCodeMappings.forEach((mapping) => {
+          if (event.currentTarget.checked) {
+            Blockly.ShortcutRegistry.registry.addKeyMapping(...mapping);
+          } else {
+            Blockly.ShortcutRegistry.registry.removeKeyMapping(...mapping);
+          }
+        });
+      });
   document.getElementById('modalButton').addEventListener('click',
       function() {
         speaker.modalToText(document.getElementById('modal-1'));
@@ -46,9 +66,5 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('replayButton').addEventListener('click',
       function() {
         speaker.modalToText(document.getElementById('modal-1'));
-      });
-  document.getElementById('setLevel').addEventListener(
-      'input', function(event) {
-        game.loadLevel(this.value);
       });
 });
