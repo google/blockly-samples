c79fa2a1541669827001a6ab396c25b59c3e7a23
Rachel Fenichel <fenichel@google.com>
2020-11-10 18:05:17 -0800

    Add skeleton for tutorial with steps

---
+++ examples/keyboard-accessibility/src/tutorial_step.js
@@ -0,0 +1,53 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview A single step in the tutorial.
+ */
+'use strict';
+
+import {speaker} from './speaker';
+/**
+ * A step in the tutorial.
+ */
+export class TutorialStep {
+  /**
+   * Class for a single step in the tutorial.
+   * @param {string} text The text to show on the modal.
+   * @param {string} textId The ID of the element where the text will be
+   *     displayed.
+   * @param {Function} doneCb The function to call when the step is completed
+   *     by the user.
+   * @constructor
+   */
+  constructor(text, textId, doneCb) {
+    /**
+     * The text being displayed to the user.
+     * @type {string}
+     */
+    this.text = text;
+
+    /**
+     * The ID of the element where the text will be displayed.
+     * @type {string}
+     */
+    this.textId = textId;
+
+    /**
+     * The function to call when the step is completed by the user.
+     * @type {Function}
+     */
+    this.doneCb = doneCb;
+  }
+
+  /**
+   * Show this step in the modal, and speak it out loud.
+   */
+  show() {
+    document.getElementById(this.textId).innerHTML = this.text;
+    speaker.speak(this.text, true);
+  }
+}
