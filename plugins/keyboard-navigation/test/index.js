8df79e02a05d8c7113cce62f5d75121505d257ae
Abby <aschmiedt@google.com>
2021-02-03 16:40:50 -0800

    Add basic files

--- plugins/keyboard-navigation/test/index.js
+++ plugins/keyboard-navigation/test/index.js
@@ -8,9 +8,13 @@
  * @fileoverview Plugin test.
  */
 
+import {createPlayground, toolboxCategories} from '@blockly/dev-tools';
 import * as Blockly from 'blockly';
-import {toolboxCategories, createPlayground} from '@blockly/dev-tools';
-import {Plugin} from '../src/index';
+
+import {LineCursor, NavigationController} from '../src';
+
+
+let controller;
 
 /**
  * Create a workspace.
@@ -20,18 +24,55 @@ import {Plugin} from '../src/index';
  */
 function createWorkspace(blocklyDiv, options) {
   const workspace = Blockly.inject(blocklyDiv, options);
-
-  // TODO: Initialize your plugin here.
-  const plugin = new Plugin(workspace);
-  plugin.init();
-
+  controller.addWorkspace(workspace);
   return workspace;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  controller = new NavigationController();
+  controller.init();
   const defaultOptions = {
     toolbox: toolboxCategories,
   };
-  createPlayground(document.getElementById('root'), createWorkspace,
-      defaultOptions);
+  createPlayground(
+      document.getElementById('root'), createWorkspace, defaultOptions);
+});
+
+document.getElementById('accessibilityModeCheck')
+    .addEventListener('click', (e) => {
+      if (e.target.checked) {
+        controller.enable(Blockly.getMainWorkspace());
+      } else {
+        controller.disable(Blockly.getMainWorkspace());
+      }
+    });
+
+document.getElementById('cursorChanger').addEventListener('change', (e) => {
+  const cursorType = e.target.value;
+  const accessibilityCheckbox =
+      document.getElementById('accessibilityModeCheck');
+  const markerManager = Blockly.getMainWorkspace().getMarkerManager();
+  const oldCurNode = markerManager.getCursor().getCurNode();
+
+  document.getElementById('cursorChanger').value = cursorType;
+  if (cursorType === 'basic') {
+    Blockly.ASTNode.NAVIGATE_ALL_FIELDS = false;
+    markerManager.setCursor(new Blockly.BasicCursor());
+  } else if (cursorType === 'line') {
+    Blockly.ASTNode.NAVIGATE_ALL_FIELDS = true;
+    markerManager.setCursor(new LineCursor());
+  } else {
+    Blockly.ASTNode.NAVIGATE_ALL_FIELDS = false;
+    markerManager.setCursor(new Blockly.Cursor());
+  }
+  if (oldCurNode) {
+    markerManager.getCursor().setCurNode(oldCurNode);
+  }
+
+  if (!accessibilityCheckbox.checked) {
+    accessibilityCheckbox.checked = true;
+    controller.enable(Blockly.getMainWorkspace());
+  }
+
+  document.activeElement.blur();
 });
