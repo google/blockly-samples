3c38ee21b5920412bb26ea6934a5306a9227984e
Maribeth Bottorff <maribethb@google.com>
2021-04-20 17:55:42 -0700

    Add a new scroll-options plugin (#676)
    * Add a new scroll-options plugin
    
    * update package description

---
+++ plugins/scroll-options/test/index.js
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Plugin test.
+ */
+
+import * as Blockly from 'blockly';
+import {toolboxCategories, createPlayground} from '@blockly/dev-tools';
+import {Plugin} from '../src/index';
+
+/**
+ * Create a workspace.
+ * @param {HTMLElement} blocklyDiv The blockly container div.
+ * @param {!Blockly.BlocklyOptions} options The Blockly options.
+ * @return {!Blockly.WorkspaceSvg} The created workspace.
+ */
+function createWorkspace(blocklyDiv, options) {
+  const workspace = Blockly.inject(blocklyDiv, options);
+
+  // TODO: Initialize your plugin here.
+  const plugin = new Plugin(workspace);
+  plugin.init();
+
+  return workspace;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const defaultOptions = {
+    toolbox: toolboxCategories,
+  };
+  createPlayground(document.getElementById('root'), createWorkspace,
+      defaultOptions);
+});
