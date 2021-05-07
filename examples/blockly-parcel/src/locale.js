a99cbbec95602b122a723678b43072d489bd80b2
Boris Ettinger <ettinger.boris@gmail.com>
2021-05-07 20:17:55 +0300

    Formatting

--- examples/blockly-parcel/src/locale.js
+++ examples/blockly-parcel/src/locale.js
@@ -1,19 +1,7 @@
 /**
  * @license
- * 
  * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 /**
@@ -25,23 +13,22 @@
 import * as Blockly from 'blockly/core';
 import 'blockly/blocks';
 import 'blockly/javascript';
-
 import * as Fr from 'blockly/msg/fr';
+
 Blockly.setLocale(Fr);
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    const workspace = Blockly.inject('blocklyDiv',
-        {
-            toolbox: document.getElementById('toolbox'),
-            media: 'media/'
-        });
+	const workspace = Blockly.inject('blocklyDiv',
+		{
+			toolbox: document.getElementById('toolbox'),
+			media: 'media/'
+		});
 
-    const lang = 'JavaScript';
-    const button = document.getElementById('blocklyButton');
-    button.addEventListener('click', function () {
-        alert("Check the console for the generated output.");
-        const code = Blockly[lang].workspaceToCode(workspace);
-        console.log(code);
-    })
+	const lang = 'JavaScript';
+	const button = document.getElementById('blocklyButton');
+	button.addEventListener('click', function () {
+		alert("Check the console for the generated output.");
+		const code = Blockly[lang].workspaceToCode(workspace);
+		console.log(code);
+	});
 });
