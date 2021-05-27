0568f87c39dcedeb4ff54428bfe60f6150d9763e
Maribeth Bottorff <maribethb@google.com>
2021-05-27 09:31:31 -0700

    Initial autoscroll

---
+++ plugins/scroll-options/src/AutoScroll.js
@@ -0,0 +1,119 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as Blockly from 'blockly/core';
+
+/**
+ * AutoScroll is used to scroll/pan the workspace automatically. For example,
+ * when a user drags a block near the edge of the workspace, it can begin
+ * automatically scrolling in that direction.
+ *
+ * Call `updateProperties` with a vector containing scroll velocity in each
+ * direction, in pixels per ms. AutoScroll will use animation frames to smoothly
+ * scroll the workspace at a constant velocity. Call `stopAndDestroy` to cancel
+ * the AutoScroll animation. You must ensure this is called eventually, or you
+ * may get stuck in an infinite animation loop and crash the browser.
+ */
+export class AutoScroll {
+  /**
+   * Creates an AutoScroll instance for a specified workspace.
+   * @param {!Blockly.WorkspaceSvg} workspace Workspace to scroll.
+   * @constructor
+   */
+  constructor(workspace) {
+    /**
+     * Workspace to scroll.
+     * @private {!Blockly.WorkspaceSvg}
+     */
+    this.workspace_ = workspace;
+
+    /**
+     * Current active vector representing scroll velocity in pixels per
+     * millisecond in each direction.
+     * @private {!Blockly.utils.Coordinate}
+     */
+    this.activeScrollVector_ = new Blockly.utils.Coordinate(0, 0);
+
+    /**
+     * ID of active requestAnimationFrame callback key.
+     * @type {number}
+     * @private
+     */
+    this.animationFrameId_ = 0;
+
+    /**
+     * Time in ms last animation frame was run.
+     * @type {number}
+     * @private
+     */
+    this.lastTime_ = Date.now();
+
+    /**
+     * Whether the scroll animation should continue. If this is false, the next
+     * animation frame will not be requested.
+     * @type {boolean}
+     * @private
+     */
+    this.shouldAnimate_ = false;
+  }
+
+  /**
+   * Cancels the current autoscroll and resets properties.
+   */
+  stopAndDestroy() {
+    this.activeScrollVector_ = new Blockly.utils.Coordinate(0, 0);
+    this.shouldAnimate_ = false;
+    cancelAnimationFrame(this.animationFrameId_);
+  }
+
+  /**
+   * Ticks scrolling behavior and triggers another
+   * frame request.
+   * @param {number} now Current time in ms. This is usually passed
+   *     automatically by `requestAnimationFrame`.
+   * @private
+   */
+  nextAnimationStep_(now) {
+    if (this.shouldAnimate_) {
+      const dt = now - this.lastTime_;
+      this.lastTime_ = now;
+      // This method could be called multiple times per ms, and we only want to
+      // scroll if we should actually move.
+      if (dt > 0) {
+        this.scrollTick_(dt);
+      }
+
+      this.animationFrameId_ =
+          requestAnimationFrame(this.nextAnimationStep_.bind(this));
+    }
+  }
+
+  /**
+   * Perform scroll given time passed.
+   * @param {number} msPassed Number of ms since last scroll tick.
+   * @private
+   */
+  scrollTick_(msPassed) {
+    const scrollDx = this.activeScrollVector_.x * msPassed;
+    const scrollDy = this.activeScrollVector_.y * msPassed;
+    this.workspace_.scrollDeltaWithAnySelectedBlock(scrollDx, scrollDy);
+  }
+
+  /**
+   * Updates the scroll vector for the current autoscroll and begins the
+   * animation.
+   * @param {!Blockly.utils.Coordinate} scrollVector New scroll velocity vector
+   *     in pixels per ms.
+   */
+  updateProperties(scrollVector) {
+    this.activeScrollVector_ = scrollVector;
+    this.shouldAnimate_ = true;
+    this.lastTime_ = Date.now();
+
+    // Start new animation
+    this.nextAnimationStep_(Date.now());
+  }
+}
--- plugins/scroll-options/src/ScrollBlockDragger.js
+++ plugins/scroll-options/src/ScrollBlockDragger.js
@@ -10,6 +10,7 @@
  */
 
 import * as Blockly from 'blockly/core';
+import {AutoScroll} from './AutoScroll';
 
 /**
  * A block dragger that adds the functionality for a block to be moved while
@@ -41,5 +42,95 @@ export class ScrollBlockDragger extends Blockly.BlockDragger {
     // TODO: This is broken. So completely broken.
     this.draggedConnectionManager_.update(
         new Blockly.utils.Coordinate(deltaXWs, deltaYWs), null);
+  }
+
+  /**
+   * May scroll the workspace as a block is dragged.
+   * If a block is dragged near the edge of the workspace, this method will
+   * cause the workspace to scroll in the direction the block is being dragged.
+   * The workspace will not resize as the block is dragged. The workspace should
+   * appear to move out from under the block, i.e., the block should stay under
+   * the user's mouse.
+   * @param {!Blockly.utils.Coordinate} newLoc New coordinate the block is being
+   *     dragged to.
+   */
+  scrollWorkspaceWhileDragging(newLoc) {
+    const SCROLL_DIRECTION_VECTORS = {
+      top: new Blockly.utils.Coordinate(0, 1),
+      bottom: new Blockly.utils.Coordinate(0, -1),
+      left: new Blockly.utils.Coordinate(1, 0),
+      right: new Blockly.utils.Coordinate(-1, 0),
+    };
+    // I just made this up, pick a better one
+    const SCROLL_SPEED = 0.2;
+
+    const candidateScrolls = [];
+    let overallScrollVector = new Blockly.utils.Coordinate(0, 0);
+
+    this.workspace_.metricsManager_.stopCalculating = true;
+    const metrics = this.workspace_.getMetrics();
+
+    // TODO(maribethb): Add fancier logic based on how far out of bounds the
+    // block is held.
+    // TODO(maribethb): This probably does not work if you change the zoom level
+
+    // See Blockly.MetricsManager for more information on the metrics used.
+    // In particular, it uses pixel coordinates where the top and left
+    // of the workspace are negative.
+    // More than one scroll vector may apply, for example if the block is
+    // dragged to a corner.
+    if (newLoc.y < metrics.viewTop) {
+      const scrollVector = SCROLL_DIRECTION_VECTORS['top'].scale(SCROLL_SPEED);
+      candidateScrolls.push(scrollVector);
+    }
+    if (newLoc.y > metrics.viewTop + metrics.viewHeight) {
+      const scrollVector =
+          SCROLL_DIRECTION_VECTORS['bottom'].scale(SCROLL_SPEED);
+      candidateScrolls.push(scrollVector);
+    }
+    if (newLoc.x < metrics.viewLeft) {
+      const scrollVector = SCROLL_DIRECTION_VECTORS['left'].scale(SCROLL_SPEED);
+      candidateScrolls.push(scrollVector);
+    }
+    if (newLoc.x > metrics.viewLeft + metrics.viewWidth) {
+      const scrollVector =
+          SCROLL_DIRECTION_VECTORS['right'].scale(SCROLL_SPEED);
+      candidateScrolls.push(scrollVector);
+    }
+
+    // Get the overall scroll direction vector (could scroll diagonally).
+    // CDO reduces down to just one vector per direction from all the possible
+    // ones they generate. Currently we just have one per direction so we don't
+    // need to do anything else.
+    candidateScrolls.forEach(function(scroll) {
+      overallScrollVector =
+          Blockly.utils.Coordinate.sum(overallScrollVector, scroll);
+    });
+
+    // If the workspace should not be scrolled any longer, cancel the
+    // autoscroll.
+    if (Blockly.utils.Coordinate.equals(
+            overallScrollVector, new Blockly.utils.Coordinate(0, 0))) {
+      this.stopAutoScrolling();
+      return;
+    }
+
+    this.activeAutoScroll_ =
+        this.activeAutoScroll_ || new AutoScroll(this.workspace_);
+    this.activeAutoScroll_.update(overallScrollVector);
+
+    this.workspace_.metricsManager_.stopCalculating = false;
+  }
+
+  /**
+   * Cancel any AutoScroll. This must be called when there is no need to scroll
+   * further, e.g., when no longer dragging near the edge of the workspace, or
+   * when no longer dragging at all.
+   */
+  stopAutoScrolling() {
+    if (this.activeAutoScroll_) {
+      this.activeAutoScroll_.stopAndDestroy();
+    }
+    this.activeAutoScroll_ = null;
   }
 }
