8495186e8fb94c0fec3a9071ce3bb1e52508182f
alschmiedt <aschmiedt@google.com>
2021-05-27 09:10:39 -0700

    Cleanup

--- plugins/block-test/test/index.js
+++ plugins/block-test/test/index.js
@@ -12,6 +12,8 @@ import * as Blockly from 'blockly';
 import {toolboxTestBlocks as toolbox,
   toolboxTestBlocksInit as onInit} from '../src/index';
 
+// Do not use the advanced playground here because it will create a circular
+// dependency with the @blockly/dev-tools package.
 document.addEventListener('DOMContentLoaded', function() {
   const workspace = Blockly.inject('root', {
     toolbox: toolbox,
