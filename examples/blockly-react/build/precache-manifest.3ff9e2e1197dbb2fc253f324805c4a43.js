e7ee29af14c7f979e24a67e8bc397f9a2c4f514c
Rachel Fenichel <fenichel@google.com>
2021-05-27 17:17:57 -0700

    Deploying 5-27-2021

---
+++ examples/blockly-react/build/precache-manifest.3ff9e2e1197dbb2fc253f324805c4a43.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2d95e70535f552c82fe013d95c0894",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9db09b0f61e12247a22d",
+    "url": "./static/css/2.2a60accc.chunk.css"
+  },
+  {
+    "revision": "460605aa9d707b2386d1",
+    "url": "./static/css/main.cceb6860.chunk.css"
+  },
+  {
+    "revision": "9db09b0f61e12247a22d",
+    "url": "./static/js/2.6219473c.chunk.js"
+  },
+  {
+    "revision": "960575ba9ac048858f1371752cc03a45",
+    "url": "./static/js/2.6219473c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460605aa9d707b2386d1",
+    "url": "./static/js/main.daec80e6.chunk.js"
+  },
+  {
+    "revision": "d599493be2f34dbb52e2a516d307058c",
+    "url": "./static/js/main.daec80e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d579192e2c6452012d84",
+    "url": "./static/js/runtime-main.d9081955.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ examples/keyboard-accessibility/build/main.c5552e5be0e5b8182ba0.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/tutorial.js":
+/*!*************************!*\
+  !*** ./src/tutorial.js ***!
+  \*************************/
+/*! exports provided: Tutorial */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/src/tutorial.js: Unterminated string constant (208:6)\\n\\n\\u001b[0m \\u001b[90m 206 | \\u001b[39m  {\\u001b[0m\\n\\u001b[0m \\u001b[90m 207 | \\u001b[39m    text\\u001b[33m:\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 208 | \\u001b[39m      \\u001b[32m'Press the down arrow key to go to the first connection on the block.,\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m     | \\u001b[39m      \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 209 | \\u001b[39m    onStart\\u001b[33m:\\u001b[39m \\u001b[36mfunction\\u001b[39m (tutorial) {\\u001b[0m\\n\\u001b[0m \\u001b[90m 210 | \\u001b[39m      console\\u001b[33m.\\u001b[39mlog(\\u001b[32m'onStart'\\u001b[39m)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 211 | \\u001b[39m      console\\u001b[33m.\\u001b[39mlog(tutorial)\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at Parser._raise (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:790:17)\\n    at Parser.raiseWithData (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:783:17)\\n    at Parser.raise (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:777:17)\\n    at Parser.readString (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:8724:20)\\n    at Parser.getTokenFromCode (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:8361:14)\\n    at Parser.nextToken (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:7888:12)\\n    at Parser.next (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:7813:10)\\n    at Parser.eat (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:7818:12)\\n    at Parser.parseObjectProperty (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:11054:14)\\n    at Parser.parseObjPropValue (/home/fenichel/blockly/blockly-samples/examples/keyboard-accessibility/node_modules/@babel/parser/lib/index.js:11080:100)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3NyYy90dXRvcmlhbC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/tutorial.js\n");
+
+/***/ }),
+
+/***/ "./src/tutorial_step.js":
+false
+
+})
