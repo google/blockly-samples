c02c95bc5a3a971c7dc2074ce8e2201b37778ae2
kozbial <kozbial@google.com>
2021-10-01 14:41:40 -0700

    Add missing import

--- plugins/block-dynamic-connection/src/index.js
+++ plugins/block-dynamic-connection/src/index.js
@@ -9,6 +9,7 @@
  *     connections that appear when a block is dragged over inputs on the block.
  */
 
+import * as Blockly from 'blockly/core';
 import './insertion_marker_manager_monkey_patch';
 import './dynamic_if.js';
 import './dynamic_text_join.js';
