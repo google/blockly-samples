780560366ffbfd9a3f2e133da64442d131299761
Maribeth Bottorff <maribethb@google.com>
2022-01-25 11:41:48 -0800

    fix: Make block-extension-tooltip build successfully (#983)
    * fix: Make tooltip plugin build successfully
    
    * fix: add todo for import
    
    * chore: make block-extension
    -tooltip private for now

--- plugins/block-extension-tooltip/src/index.ts
+++ plugins/block-extension-tooltip/src/index.ts
@@ -8,7 +8,11 @@
  * @fileoverview Custom tooltip block extension.
  * @author samelh@google.com (Sam El-Husseini)
  */
-import * as Blockly from 'blockly/core';
+
+// TODO(maribethb): This should be from 'blockly/core'; fix asap when this
+// plugin is made compatible with v7 of Blockly.
+// See https://github.com/google/blockly-samples/issues/805
+import * as Blockly from 'blockly';
 import './tooltip_monkey_patch';
 
 type TooltipRender = (block: Blockly.BlockSvg) => HTMLElement;
