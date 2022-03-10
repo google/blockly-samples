2c6736817f95f51df578bb916ac6351a9768b2c9
Christopher Allen <cpcallen+git@google.com>
2022-03-10 16:47:04 +0000

    refactor: Introduce Renamer class
    The intention is to encapsulate the digested renamings so that
    they can be applied to multiple files without needing to load
    all those files in to memory simultaneously.
    
    Retain doRenamings function temporarily so as not to break
    the tests.

--- plugins/migration/bin/rename.js
+++ plugins/migration/bin/rename.js
@@ -48,68 +48,91 @@ export async function getDatabase() {
  * @param {string} newVersion The version to migrate to.
  * @param {!Array<string>} strings The strings to apply the renamings in.
  * @return {!Array<string>} The strings with renamings applied.
+ * @deprecated Use Renamer instance instead.
  */
 export function doRenamings(database, currVersion, newVersion, strings) {
-  const renamings = calculateRenamings(database, currVersion, newVersion);
-  return strings.map((str) => applyRenamings(renamings, str));
+  const renamer = new Renamer(database, currVersion, newVersion);
+  return strings.map((str) => renamer.rename(str));
 }
 
 /**
- * Turns the database of renames into an intermediate format that is easier
- * to apply.
+ * An object which can apply a given set of renamings to the contents
+ * of JavaScript files, so as to automatically update developer's code
+ * where parts of the Blockly API have been renamed.
  * @param {!Object} database The database of renames.
  * @param {string} currVersion The version to migrate from.
  * @param {string} newVersion The version to migrate to.
- * @return {*} The collection of renamings to perform.
+ * @param {!Array<string>} strings The strings to apply the renamings in.
+ * @return {!Array<string>} The strings with renamings applied.
  */
-function calculateRenamings(database, currVersion, newVersion) {
-  const renamings = [];
+export class Renamer {
+  /**
+   * @param {!Object} database The database of renames.
+   * @param {string} currVersion The version to migrate from.
+   * @param {string} newVersion The version to migrate to.
+   */
+  constructor(database, currVersion, newVersion) {
+    this.renamings =
+        Renamer.calculateRenamings(database, currVersion, newVersion);
+  }
 
-  // Sort versions (case not already sorted), as we want to apply
-  // renamings in order.
-  const versions = Object.keys(database).sort(compareVersions);
-  for (const version of versions) {
-    // Only process versions in the range (currVersion, newVersion].
-    if (compareVersions.compare(version, currVersion, '<=')) continue;
-    if (compareVersions.compare(version, newVersion, '>')) break;
+  /**
+   * Turns a database of renamings into an intermediate format that is
+   * easier to apply.
+   * @param {!Object} database The database of renames.
+   * @param {string} currVersion The version to migrate from.
+   * @param {string} newVersion The version to migrate to.
+   * @return {*} The collection of renamings to perform.
+   */
+  static calculateRenamings(database, currVersion, newVersion) {
+    const renamings = [];
 
-    for (const module of database[version]) {
-      const oldModulePath = module.oldPath ?? module.oldName;
-      const newModulePath = module.newPath ?? module.newName ?? oldModulePath;
+    // Sort versions (case not already sorted), as we want to apply
+    // renamings in order.
+    const versions = Object.keys(database).sort(compareVersions);
+    for (const version of versions) {
+      // Only process versions in the range (currVersion, newVersion].
+      if (compareVersions.compare(version, currVersion, '<=')) continue;
+      if (compareVersions.compare(version, newVersion, '>')) break;
 
-      if (module.exports) {
-        for (const [oldExportName, info] of Object.entries(module.exports)) {
-          const oldExportPath =
-              info.oldPath ?? `${oldModulePath}.${oldExportName}`;
-          const newExportPath = info.newPath ??
-              (info.newModule ?? newModulePath) + '.' +
-              (info.newExport ?? oldExportName);
-          if (newExportPath !== oldExportPath) {
-            renamings.push({old: oldExportPath, new: newExportPath});
+      for (const module of database[version]) {
+        const oldModulePath = module.oldPath ?? module.oldName;
+        const newModulePath = module.newPath ?? module.newName ?? oldModulePath;
+
+        if (module.exports) {
+          for (const [oldExportName, info] of Object.entries(module.exports)) {
+            const oldExportPath =
+                info.oldPath ?? `${oldModulePath}.${oldExportName}`;
+            const newExportPath = info.newPath ??
+                (info.newModule ?? newModulePath) + '.' +
+                (info.newExport ?? oldExportName);
+            if (newExportPath !== oldExportPath) {
+              renamings.push({old: oldExportPath, new: newExportPath});
+            }
           }
         }
-      }
 
-      if (newModulePath !== oldModulePath) {
-        renamings.push({old: oldModulePath, new: newModulePath});
+        if (newModulePath !== oldModulePath) {
+          renamings.push({old: oldModulePath, new: newModulePath});
+        }
       }
     }
+    return renamings;
   }
-  return renamings;
-}
 
-/**
- * Applies the given renamings directly to developers' files.
- * @param {*} renamings The collection of renamings to perform.
- * @param {string} str The string to apply the renamings in.
- * @return {string} The file with renamings applied.
- */
-function applyRenamings(renamings, str) {
-  // Quick hack to test calculateRenamings.
-  for (const entry of renamings) {
-    if (str.startsWith(entry.old)) {
-      return entry.new + str.slice(entry.old.length);
+  /**
+   * Applies the given renamings directly to a JavaScript string
+   * (presumably the contents of a developer's file).
+   * @param {string} str The string to apply the renamings in.
+   * @return {string} The file with renamings applied.
+   */
+  rename(str) {
+    // Quick hack to test calculateRenamings.
+    for (const entry of this.renamings) {
+      if (str.startsWith(entry.old)) {
+        return entry.new + str.slice(entry.old.length);
+      }
     }
+    return str;
   }
-  return str;
 }
