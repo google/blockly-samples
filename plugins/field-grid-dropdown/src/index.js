c95c36153213b1e71f5a1a7f6574d01f94e6d59b
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2022-06-21 16:36:14 -0400

    fix!: use multiline string instead of array for Blockly.Css (#1171)
    * Use multiline string without array
    
    * fix lint error
    
    * bump Blockly version

--- plugins/field-grid-dropdown/src/index.js
+++ plugins/field-grid-dropdown/src/index.js
@@ -139,9 +139,8 @@ Blockly.fieldRegistry.register('field_grid_dropdown', FieldGridDropdown);
 /**
  * CSS for slider field.
  */
-Blockly.Css.register([
-  /* eslint-disable indent */
-  `/** Setup grid layout of DropDown */
+Blockly.Css.register(
+    `/** Setup grid layout of DropDown */
   .fieldGridDropDownContainer.blocklyMenu {
       display: grid;
       grid-gap: 7px;
@@ -171,6 +170,5 @@ Blockly.Css.register([
   }
   .fieldGridDropDownContainer {
     margin: 7px; /* needed for highlight */
-  }`,
-  /* eslint-enable indent */
-]);
+  }`
+);
