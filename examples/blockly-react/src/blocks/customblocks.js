3a317f8f75befee47ae7abe39d1407a1b0b3c5bb
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-06-28 14:04:47 +0100

    chore: deleted unnecessary comments

--- examples/blockly-react/src/blocks/customblocks.js
+++ examples/blockly-react/src/blocks/customblocks.js
@@ -74,24 +74,3 @@ Blockly.Blocks['test_react_field'] = {
     this.setStyle('loop_blocks');
   }
 };
-
-// var reactDateField = {
-//   "type": "test_react_date_field",
-//   "message0": "date field %1",
-//   "args0": [
-//     {
-//       "type": "field_react_date",
-//       "name": "DATE",
-//       "date": "01/01/2020"
-//     },
-//   ],
-//   "previousStatement": null,
-//   "nextStatement": null,
-// };
-
-// Blockly.Blocks['test_react_date_field'] = {
-//   init: function() {
-//     this.jsonInit(reactDateField);
-//     this.setStyle('loop_blocks');
-//   }
-// };
