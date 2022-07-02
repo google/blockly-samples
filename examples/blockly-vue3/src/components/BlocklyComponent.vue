a84f007874cf71dc6144277726f0139c9fbc2d66
e-gloo <dylan.coodien@epitech.eu>
2022-07-02 21:06:24 +0200

    Add vue3 blockly sample

---
+++ examples/blockly-vue3/src/components/BlocklyComponent.vue
@@ -0,0 +1,42 @@
+<script setup>
+/**
+ * @fileoverview Blockly Vue Component.
+ * @author dcodien@gmail.com (Dylan Coodien)
+ */
+
+import { onMounted, ref, shallowRef } from "vue";
+import Blockly from "blockly";
+
+const props = defineProps(["options"]);
+const blocklyToolbox = ref();
+const blocklyDiv = ref();
+const workspace = shallowRef();
+
+defineExpose({ workspace });
+
+onMounted(() => {
+  const options = props.options || {};
+  if (!options.toolbox) {
+    options.toolbox = blocklyToolbox.value;
+  }
+  workspace.value = Blockly.inject(blocklyDiv.value, options);
+});
+</script>
+
+<template>
+  <div>
+    <div class="blocklyDiv" ref="blocklyDiv"></div>
+    <xml ref="blocklyToolbox" style="display: none">
+      <slot></slot>
+    </xml>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.blocklyDiv {
+  height: 100%;
+  width: 100%;
+  text-align: left;
+}
+</style>
