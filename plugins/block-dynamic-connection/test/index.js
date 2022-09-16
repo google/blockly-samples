428c81cef4bb58ce99128cbb78f1930ad238b6e5
Yamini <uyamini1996@gmail.com>
2022-09-16 16:07:10 -0500

    fix: Update block-dynamic-connection to use JSON toolbox (#1238) (#1286)
    * Update index.js
    
    fix: Issue 1238 solved
    
    * fix: Update block-dynamic-connection to use JSON toolbox (#1238)
    
    * fix: lint issue in the root directory

--- plugins/block-dynamic-connection/test/index.js
+++ plugins/block-dynamic-connection/test/index.js
@@ -25,8 +25,61 @@ function createWorkspace(blocklyDiv, options) {
 document.addEventListener('DOMContentLoaded', function() {
   BlockDynamicConnection.overrideOldBlockDefinitions();
 
+  const toolbox = {
+    kind: 'flyoutToolbox',
+    contents: [
+      {
+        kind: 'block',
+        type: 'text_join',
+      },
+      {
+        kind: 'block',
+        type: 'lists_create_with',
+      },
+      {
+        kind: 'block',
+        type: 'controls_if',
+      },
+      {
+        kind: 'block',
+        type: 'logic_boolean',
+        fields: {
+          BOOL: 'TRUE',
+        },
+      },
+      {
+        kind: 'block',
+        type: 'text',
+        fields: {
+          TEXT: 'abc',
+        },
+      },
+      {
+        kind: 'block',
+        type: 'math_number',
+        fields: {
+          NUM: '123',
+        },
+      },
+      {
+        kind: 'block',
+        type: 'text_print',
+        inputs: {
+          TEXT: {
+            shadow: {
+              type: 'text',
+              fields: {
+                TEXT: 'abc',
+              },
+            },
+          },
+        },
+      },
+    ],
+  };
+
   const defaultOptions = {
-    toolbox: document.getElementById('toolbox'),
+    toolbox,
   };
   createPlayground(document.getElementById('root'), createWorkspace,
       defaultOptions);
