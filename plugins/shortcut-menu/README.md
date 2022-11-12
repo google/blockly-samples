f4f169a92bc0b0b42026b7b72b8c611a71630e2e
Rickson Yang <rixin.yang.2001@gmail.com>
2022-11-11 19:35:54 -0500

    feat: shortcut-menu plugin init

---
+++ plugins/shortcut-menu/README.md
@@ -0,0 +1,46 @@
+# blockly-plugin-shortcut-menu [![Built on Blockly](https://tinyurl.com/built-on-blockly)](https://github.com/google/blockly)
+
+<!--
+  - TODO: Edit plugin description.
+  -->
+A [Blockly](https://www.npmjs.com/package/blockly) plugin that ...
+
+## Installation
+
+### Yarn
+```
+yarn add blockly-plugin-shortcut-menu
+```
+
+### npm
+```
+npm install blockly-plugin-shortcut-menu --save
+```
+
+## Usage
+
+<!--
+  - TODO: Update usage.
+  -->
+```js
+import * as Blockly from 'blockly';
+import {Plugin} from 'blockly-plugin-shortcut-menu';
+
+// Inject Blockly.
+const workspace = Blockly.inject('blocklyDiv', {
+  toolbox: toolboxCategories,
+});
+
+// Initialize plugin.
+const plugin = new Plugin(workspace);
+plugin.init();
+```
+
+## API
+
+<!--
+  - TODO: describe the API.
+  -->
+
+## License
+Apache 2.0
---
+++ plugins/shortcut-menu/test/index.js
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Plugin test.
+ */
+
+import * as Blockly from 'blockly';
+import {toolboxCategories, createPlayground} from '@blockly/dev-tools';
+import {Plugin} from '../src/index';
+
+/**
+ * Create a workspace.
+ * @param {HTMLElement} blocklyDiv The blockly container div.
+ * @param {!Blockly.BlocklyOptions} options The Blockly options.
+ * @return {!Blockly.WorkspaceSvg} The created workspace.
+ */
+function createWorkspace(blocklyDiv, options) {
+  const workspace = Blockly.inject(blocklyDiv, options);
+
+  // TODO: Initialize your plugin here.
+  const plugin = new Plugin('Short Cut Menu', workspace);
+  plugin.init();
+  plugin.show();
+
+  return workspace;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const defaultOptions = {
+    toolbox: toolboxCategories,
+  };
+  createPlayground(
+      document.getElementById('root'),
+      createWorkspace,
+      defaultOptions
+  );
+});
