a3a764e17ea44c84b4fa86008644c8e66970e501
Beka Westberg <bwestberg@google.com>
2023-01-31 21:05:31 +0000

    chore: work on adding blocks

--- plugins/block-sharable-procedures/src/index.ts
+++ plugins/block-sharable-procedures/src/index.ts
@@ -4,10 +4,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as Blockly from 'blockly/core';
 import {ObservableParameterModel} from './observable_parameter_model';
 import {ObservableProcedureModel} from './observable_procedure_model';
+import {blocks} from './blocks';
+
+console.log(blocks);
 
 export {
   ObservableParameterModel,
   ObservableProcedureModel,
+  blocks,
 };
+
+export function unregisterProcedureBlocks() {
+  delete Blockly.Blocks['procedures_defnoreturn'];
+  delete Blockly.Blocks['procedures_callnoreturn'];
+  delete Blockly.Blocks['procedures_defreturn'];
+  delete Blockly.Blocks['procedures_callreturn'];
+}
