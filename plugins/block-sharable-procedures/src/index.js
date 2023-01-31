69b67a18944072bc7406579533cb2ad6964f53d7
Beka Westberg <bwestberg@google.com>
2023-01-31 11:16:22 -0800

    chore: add new plugin package for procedure blocks (#1523)
    * chore: add new plugin package for procedure blocks
    
    * chore: remove tests
    
    * chore: rename backed-procedures to sharable-procedures
    
    * chore: PR comments
    
    * chore: add plugin to main readme
    
    * chore: format

---
+++ plugins/block-sharable-procedures/src/index.js
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// TODO: Edit block overview.
+/**
+ * @fileoverview Block overview.
+ */
+
+import Blockly from 'blockly/core';
+
+// TODO: Update block definition.
+Blockly.defineBlocksWithJsonArray([
+  {
+    'type': 'block_template',
+    'message0': 'block template',
+    'style': 'math_blocks',
+  },
+]);
---
+++ plugins/block-sharable-procedures/test/block_test.mocha.js
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+require('../src/index');
+
+suite('BlockTemplate', function() {
+});
