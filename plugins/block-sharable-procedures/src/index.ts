c190eca57865090d9b6027ae4af7eecf425396e8
Beka Westberg <bwestberg@google.com>
2023-02-21 15:22:49 -0800

    feat: add procedure blocks (#1544)
    * chore: work on adding blocks
    
    * chore: add procedure block tests
    
    * chore: format
    
    * chore: add missing reexports
    
    * chore: add serialization tests
    
    * chore: move deserialization test suite
    
    * chore: add tests for getting callers + defs, and remove backwards compatibility stuff
    
    * chore: remove useless opt_paramId parameter
    
    * chore: fix ticking the clock in tests
    
    * chore: fix properly using the sinon sandbox
    
    * chore: remove console log

--- plugins/block-sharable-procedures/src/index.ts
+++ plugins/block-sharable-procedures/src/index.ts
@@ -4,10 +4,31 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as Blockly from 'blockly/core';
+import {blocks} from './blocks';
+import {IProcedureBlock, isProcedureBlock} from './i_procedure_block';
 import {ObservableParameterModel} from './observable_parameter_model';
 import {ObservableProcedureModel} from './observable_procedure_model';
+import {triggerProceduresUpdate} from './update_procedures';
 
 export {
+  blocks,
+  IProcedureBlock,
+  isProcedureBlock,
   ObservableParameterModel,
   ObservableProcedureModel,
+  triggerProceduresUpdate,
 };
+
+/**
+ * Unregisters all of the procedure blocks.
+ *
+ * Usually used to unregister the built-in blocks, before register the
+ * blocks provided by this plugin.
+ */
+export function unregisterProcedureBlocks() {
+  delete Blockly.Blocks['procedures_defnoreturn'];
+  delete Blockly.Blocks['procedures_callnoreturn'];
+  delete Blockly.Blocks['procedures_defreturn'];
+  delete Blockly.Blocks['procedures_callreturn'];
+}
