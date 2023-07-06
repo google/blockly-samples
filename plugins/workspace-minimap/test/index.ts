2c5db99f2be384ccb208cb7b49d193101b30baa1
Cesar Ades <cesarades@google.com>
2023-07-06 22:50:41 +0000

    updated test file for styling purposes

--- plugins/workspace-minimap/test/index.ts
+++ plugins/workspace-minimap/test/index.ts
@@ -13,22 +13,21 @@ import {toolboxCategories, createPlayground} from '@blockly/dev-tools';
 import {Minimap, PositionedMinimap} from '../src/index';
 
 // Creates the primary workspace and adds the minimap.
-const ws = Blockly.inject('primaryDiv', {toolbox: toolboxCategories});
-const mp = new Minimap(ws);
-mp.init();
+const workspace = Blockly.inject('primaryDiv', {toolbox: toolboxCategories});
+const minimap = new Minimap(workspace);
+minimap.init();
 
-// Creates 100 random blocks
+// Creates 100 if blocks
 for (let i = 0; i < 100; i++) {
-  const prototype = 'controls_if';
-  const block = ws.newBlock(prototype);
+  const block = workspace.newBlock('controls_if');
   block.initSvg();
   block.render();
 }
 
 // Move the blocks to random locations.
-for (const block of ws.getAllBlocks(true)) {
+for (const block of workspace.getAllBlocks(true)) {
   const cord = new Blockly.utils.Coordinate(
       Math.round(Math.random() * 450 + 40),
       Math.round(Math.random() * 600 + 40));
-  block.moveTo(cord, ['drag']);
+  block.moveTo(cord);
 }
