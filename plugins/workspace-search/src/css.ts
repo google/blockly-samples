d0e1075888d65411dd9ddcc45db5cd4dfa152da7
Aaron Dodson <adodson@google.com>
2023-08-14 10:55:55 -0700

    refactor: Improve handling of multiline CSS text

--- plugins/workspace-search/src/css.ts
+++ plugins/workspace-search/src/css.ts
@@ -12,9 +12,8 @@
 
 /**
  * Base64 encoded data uri for close icon.
- * @type {string}
  */
-const CLOSE_SVG_DATAURI =
+const closeSvgDataUri =
     'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC' +
     '9zdmciIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjE0Ij48cGF0aC' +
     'BkPSJNMTkgNi40MUwxNy41OSA1IDEyIDEwLjU5IDYuNDEgNSA1IDYuNDEgMTAuNTkgMTIgNS' +
@@ -23,9 +22,8 @@ const CLOSE_SVG_DATAURI =
 
 /**
  * Base64 encoded data uri for keyboard arrow down icon.
- * @type {string}
  */
-const ARROW_DOWN_SVG_DATAURI =
+const arrowDownSvgDataUri =
     'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC' +
     '9zdmciIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjE0Ij48cGF0aC' +
     'BkPSJNNy40MSA4LjU5TDEyIDEzLjE3bDQuNTktNC41OEwxOCAxMGwtNiA2LTYtNiAxLjQxLT' +
@@ -33,9 +31,8 @@ const ARROW_DOWN_SVG_DATAURI =
 
 /**
  * Base64 encoded data uri for keyboard arrow up icon.
- * @type {string}
  */
-const ARROW_UP_ARROW_SVG_DATAURI =
+const arrowUpSvgDataUri =
     'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC' +
     '9zdmciIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjE0Ij48cGF0aC' +
     'BkPSJNNy40MSAxNS40MUwxMiAxMC44M2w0LjU5IDQuNThMMTggMTRsLTYtNi02IDZ6Ii8+PH' +
@@ -43,51 +40,47 @@ const ARROW_UP_ARROW_SVG_DATAURI =
 
 /**
  * CSS for workspace search.
- * @type {Array.<string>}
  */
-const CSS_CONTENT = [
-  /* eslint-disable indent */
-  'path.blocklyPath.blockly-ws-search-highlight {',
-    'fill: #000;',
-  '}',
-  'path.blocklyPath.blockly-ws-search-highlight.blockly-ws-search-current {',
-    'fill: grey;',
-  '}',
-  '.blockly-ws-search-close-btn {',
-    'background: url(' + CLOSE_SVG_DATAURI + ') no-repeat top left;',
-  '}',
-  '.blockly-ws-search-next-btn {',
-    'background: url(' + ARROW_DOWN_SVG_DATAURI + ') no-repeat top left;',
-  '}',
-  '.blockly-ws-search-previous-btn {',
-    'background: url(' +ARROW_UP_ARROW_SVG_DATAURI + ') no-repeat top left;',
-  '}',
-  '.blockly-ws-search {',
-    'background: #fff;',
-    'border: solid lightgrey 0.5px;',
-    'box-shadow: 0px 10px 20px grey;',
-    'justify-content: center;',
-    'padding: 0.25em;',
-    'position: absolute;',
-    'z-index: 70;',
-  '}',
-  '.blockly-ws-search-input input {',
-    'border: none;',
-  '}',
-  '.blockly-ws-search button {',
-    'border: none;',
-  '}',
-  '.blockly-ws-search-actions {',
-    'display: flex;',
-  '}',
-  '.blockly-ws-search-container {',
-    'display: flex;',
-  '}',
-  '.blockly-ws-search-content {',
-    'display: flex;',
-  '}',
-  /* eslint-enable indent */
-];
+const cssContent =
+  `path.blocklyPath.blockly-ws-search-highlight {
+    fill: #000;
+  }
+  path.blocklyPath.blockly-ws-search-highlight.blockly-ws-search-current {
+    fill: grey;
+  }
+  .blockly-ws-search-close-btn {
+    background: url(${closeSvgDataUri}) no-repeat top left;
+  }
+  .blockly-ws-search-next-btn {
+    background: url(${arrowDownSvgDataUri}) no-repeat top left;
+  }
+  .blockly-ws-search-previous-btn {
+    background: url(${arrowUpSvgDataUri}) no-repeat top left;
+  }
+  .blockly-ws-search {
+    background: #fff;
+    border: solid lightgrey 0.5px;
+    box-shadow: 0px 10px 20px grey;
+    justify-content: center;
+    padding: 0.25em;
+    position: absolute;
+    z-index: 70;
+  }
+  .blockly-ws-search-input input {
+    border: none;
+  }
+  .blockly-ws-search button {
+    border: none;
+  }
+  .blockly-ws-search-actions {
+    display: flex;
+  }
+  .blockly-ws-search-container {
+    display: flex;
+  }
+  .blockly-ws-search-content {
+    display: flex;
+  }`;
 
 /**
  * Injects CSS for workspace search.
@@ -100,11 +93,10 @@ export const injectSearchCss = (function() {
       return;
     }
     executed = true;
-    const text = CSS_CONTENT.join('\n');
     // Inject CSS tag at start of head.
     const cssNode = document.createElement('style');
     cssNode.id = 'blockly-ws-search-style';
-    const cssTextNode = document.createTextNode(text);
+    const cssTextNode = document.createTextNode(cssContent);
     cssNode.appendChild(cssTextNode);
     document.head.insertBefore(cssNode, document.head.firstChild);
   };
