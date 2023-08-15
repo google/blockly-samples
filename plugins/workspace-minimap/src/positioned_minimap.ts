200701e5407aa77295a2c57d20f245b0b96a566b
Cesar Ades <51796383+cesarades@users.noreply.github.com>
2023-08-15 14:13:36 -0700

    chore: Unit testing on minimap position (#1853)
    * suite scaffold
    
    * extensive test cases (scaffold)
    
    * options
    
    * square one
    
    * LTR done
    
    * RTL done
    
    * feedback refactoring
    
    * split method for unit testing
    
    * added internal tag
    
    * rounding feedback

--- plugins/workspace-minimap/src/positioned_minimap.ts
+++ plugins/workspace-minimap/src/positioned_minimap.ts
@@ -74,8 +74,31 @@ export class PositionedMinimap extends Minimap implements Blockly.IPositionable 
      */
     position(metrics: Blockly.MetricsManager.UiMetrics,
         savedPositions: Blockly.utils.Rect[]): void {
-      this.resize();
+      this.setSize();
+      this.setPosition(metrics, savedPositions);
+      this.setAttributes();
+    }
 
+
+    /**
+     * Sizes the minimap.
+     * @internal
+     */
+    setSize(): void {
+      const viewWidth = this.primaryWorkspace.getMetrics().viewWidth;
+      this.width = Math.max(minWidth, viewWidth / 5);
+      this.height = this.width * 2 / 3;
+    }
+
+
+    /**
+     * Calculates the position of the minimap over the primary workspace.
+     * @param metrics The workspace metrics.
+     * @param savedPositions List of rectangles already on the workspace.
+     * @internal
+     */
+    setPosition(metrics: Blockly.MetricsManager.UiMetrics,
+        savedPositions: Blockly.utils.Rect[]): void {
       // Aliases.
       const workspace = this.primaryWorkspace;
       const scrollbars = workspace.scrollbar;
@@ -130,26 +153,13 @@ export class PositionedMinimap extends Minimap implements Blockly.IPositionable 
           i = -1;
         }
       }
-
-      // Update the Css.
-      this.setMinimapCss();
     }
 
 
     /**
-     * Sizes the minimap.
+     * Sets the CSS attribute for the minimap.
      */
-    resize(): void {
-      const viewWidth = this.primaryWorkspace.getMetrics().viewWidth;
-      this.width = Math.max(minWidth, viewWidth / 5);
-      this.height = this.width * 2 / 3;
-    }
-
-
-    /**
-     * Updates the CSS attribute for the minimap.
-     */
-    private setMinimapCss(): void {
+    private setAttributes(): void {
       const injectDiv = this.minimapWorkspace.getInjectionDiv();
       // TODO: Styling properties will be added later this is a placeholder.
       injectDiv.parentElement.setAttribute('style',
