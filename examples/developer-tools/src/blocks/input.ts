7e599be90060268c83df98a261d4bc16b9996e2c
Maribeth Bottorff <maribethb@google.com>
2023-09-19 13:27:29 -0700

    feat: type input in certain inputs only

--- examples/developer-tools/src/blocks/input.ts
+++ examples/developer-tools/src/blocks/input.ts
@@ -12,7 +12,7 @@ import * as Blockly from 'blockly';
  * Highly inefficient (On^2), but n is small.
  * @param referenceBlock Block to check.
  */
-function inputNameCheck(referenceBlock: Blockly.Block) {
+const inputNameCheck = function(referenceBlock: Blockly.Block) {
   if (!referenceBlock.workspace) {
     // Block has been deleted.
     return;
@@ -30,80 +30,52 @@ function inputNameCheck(referenceBlock: Blockly.Block) {
   const msg = (count > 1) ?
       'There are ' + count + ' input blocks\n with this name.' : null;
   referenceBlock.setWarningText(msg);
-}
+};
+
+// Inputs that should take a "type" input for connection checks
+const inputsWithTypeInputs = new Set(['input_value', 'input_statement']);
+
+const inputTypeValidator = function(value: string): string {
+  if (inputsWithTypeInputs.has(value)) {
+    if (!this.getInput('TYPE')) {
+      this.appendValueInput('TYPE')
+          .setCheck(null)
+          .setAlign(Blockly.inputs.Align.RIGHT)
+          .appendField('type');
+    }
+  } else {
+    if (this.getInput('TYPE')) {
+      this.removeInput('TYPE');
+    }
+  }
+
+  return value;
+};
 
 export const input = {
-  init: function() {
-    this.jsonInit({
-      'type': 'block_type',
-      'message0': '%1 input %2 %3 fields %4 %5 type %6',
-      'args0': [
-        {
-          'type': 'field_dropdown',
-          'name': 'INPUT_TYPE',
-          'options': [
-            [
-              'value',
-              'input_value',
-            ],
-            [
-              'statement',
-              'input_statement',
-            ],
-            [
-              'dummy',
-              'input_dummy',
-            ],
-            [
-              'end-row',
-              'input_end_row',
-            ],
-          ],
-        },
-        {
-          'type': 'field_input',
-          'name': 'INPUTNAME',
-          'text': 'NAME',
-        },
-        {
-          'type': 'input_dummy',
-        },
-        {
-          'type': 'field_dropdown',
-          'name': 'ALIGNMENT',
-          'options': [
-            [
-              'left',
-              'LEFT',
-            ],
-            [
-              'right',
-              'RIGHT',
-            ],
-            [
-              'centre',
-              'CENTRE',
-            ],
-          ],
-        },
-        {
-          'type': 'input_statement',
-          'name': 'FIELDS',
-          'check': 'Field',
-        },
-        {
-          'type': 'input_value',
-          'name': 'TYPE',
-          'align': 'RIGHT',
-          'check': ['Type', 'TypeArray'],
-        },
-      ],
-      'previousStatement': 'Input',
-      'nextStatement': 'Input',
-      'colour': 210,
-      'tooltip': 'A value socket for horizontal connections.',
-      'helpUrl': 'https://www.youtube.com/watch?v=s2_xaEvcVI0#t=71',
-    });
+  init: function(this: Blockly.Block) {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([
+          ['value', 'input_value'],
+          ['statement', 'input_statement'],
+          ['dummy', 'input_dummy'],
+          ['end-row', 'input_end_row'],
+        ], inputTypeValidator.bind(this)), 'INPUT_TYPE')
+        .appendField('input')
+        .appendField(new Blockly.FieldTextInput('NAME'), 'INPUTNAME');
+    this.appendStatementInput('FIELDS')
+        .setCheck(null)
+        .appendField('fields')
+        .appendField(new Blockly.FieldDropdown([
+          ['left', 'LEFT'],
+          ['right', 'RIGHT'],
+          ['centre', 'CENTRE'],
+        ]), 'ALIGNMENT');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
   },
   onchange: function() {
     inputNameCheck(this);
