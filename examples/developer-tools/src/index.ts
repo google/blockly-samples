6cd7525a692460d076bafc998d65acb4e29b0c5a
Maribeth Bottorff <maribethb@google.com>
2023-09-20 14:23:03 -0700

    feat: save the current block to local storage

--- examples/developer-tools/src/index.ts
+++ examples/developer-tools/src/index.ts
@@ -9,6 +9,7 @@ import {registerAllBlocks} from './blocks';
 import {toolbox} from './toolbox';
 import './index.css';
 import {jsonDefinitionGenerator} from './output-generators/json_definition_generator';
+import {load, save} from './serialization';
 
 // Put Blockly in the global scope for easy debugging.
 (window as any).Blockly = Blockly;
@@ -25,53 +26,11 @@ const mainWorkspace = Blockly.inject(mainWorkspaceDiv, {toolbox});
 // Disable orphan blocks on the main workspace
 mainWorkspace.addChangeListener(Blockly.Events.disableOrphans);
 
+// Load either saved state or new state
+load(mainWorkspace);
 
-const startBlocks = {
-  'blocks': {
-    'languageVersion': 0,
-    'blocks': [
-      {
-        'type': 'factory_base',
-        'deletable': false,
-        'x': 53,
-        'y': 23,
-        'fields': {
-          'NAME': 'block_type',
-          'INLINE': 'AUTO',
-          'CONNECTIONS': 'NONE',
-        },
-        'inputs': {
-          'TOOLTIP': {
-            'block': {
-              'type': 'text',
-              'deletable': false,
-              'movable': false,
-              'fields': {
-                'TEXT': '',
-              },
-            },
-          },
-          'HELPURL': {
-            'block': {
-              'type': 'text',
-              'deletable': false,
-              'movable': false,
-              'fields': {
-                'TEXT': '',
-              },
-            },
-          },
-        },
-      },
-    ],
-  },
-};
-
-Blockly.serialization.workspaces.load(startBlocks, mainWorkspace);
 
 let blockName = '';
-
-
 // Whenever the workspace changes meaningfully, update the preview.
 mainWorkspace.addChangeListener((e: Blockly.Events.Abstract) => {
   // Don't run the code when the workspace finishes loading; we're
@@ -82,6 +41,7 @@ mainWorkspace.addChangeListener((e: Blockly.Events.Abstract) => {
     return;
   }
 
+  save(mainWorkspace);
   previewWorkspace.clear();
 
   // If we previously had a block registered, delete it.
---
+++ examples/developer-tools/src/serialization.ts
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as Blockly from 'blockly/core';
+
+const storageKey = 'blockFactory';
+
+const startBlocks = {
+  'blocks': {
+    'languageVersion': 0,
+    'blocks': [
+      {
+        'type': 'factory_base',
+        'deletable': false,
+        'x': 53,
+        'y': 23,
+        'fields': {
+          'NAME': 'block_type',
+          'INLINE': 'AUTO',
+          'CONNECTIONS': 'NONE',
+        },
+        'inputs': {
+          'TOOLTIP': {
+            'block': {
+              'type': 'text',
+              'deletable': false,
+              'movable': false,
+              'fields': {
+                'TEXT': '',
+              },
+            },
+          },
+          'HELPURL': {
+            'block': {
+              'type': 'text',
+              'deletable': false,
+              'movable': false,
+              'fields': {
+                'TEXT': '',
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
+};
+
+/**
+ * Saves the state of the workspace to browser's local storage.
+ * @param workspace Blockly workspace to save.
+ */
+export const save = function(workspace: Blockly.Workspace) {
+  const data = Blockly.serialization.workspaces.save(workspace);
+  window.localStorage?.setItem(storageKey, JSON.stringify(data));
+};
+
+/**
+ * Loads saved state from local storage into the given workspace.
+ * @param workspace Blockly workspace to load into.
+ */
+export const load = function(workspace: Blockly.Workspace) {
+  const data = window.localStorage?.getItem(storageKey);
+  if (data) {
+    Blockly.serialization.workspaces.load(JSON.parse(data), workspace);
+  } else {
+    // Load starter block
+    Blockly.serialization.workspaces.load(startBlocks, workspace);
+  }
+};
