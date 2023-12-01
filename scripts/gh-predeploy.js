7ac7daec375ce5d56719e2c16c47c723dc6b41b1
Neil Fraser <fraser@google.com>
2023-12-01 14:57:17 +0100

    Formatting
    Revert this line to verify it is this line, and not the earlier whitespace changes.

--- scripts/gh-predeploy.js
+++ scripts/gh-predeploy.js
@@ -595,11 +595,7 @@ function createIndexPage(isLocal) {
 </html>
 `;
 
-  let contents = injectHeader(
-    indexBase,
-    'Blockly Plugins &amp; Demos',
-    isLocal
-  );
+  let contents = injectHeader(indexBase, 'Blockly Plugins & Demos', isLocal);
   contents = injectFooter(contents);
   const outputPath = path.join('gh-pages', 'index.html');
   fs.writeFileSync(outputPath, contents, 'utf-8');
