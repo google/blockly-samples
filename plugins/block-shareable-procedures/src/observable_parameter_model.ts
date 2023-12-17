01fbbc704df478ca79e1345269e0c659bbef1035
shivalipatel6 <shivalipatel6@gmail.com>
2023-12-17 06:41:08 -0500

    feat: created a call to createBackingVariable in constructor This will change my last commit from a breaking change to a non-breaking change.

--- plugins/block-shareable-procedures/src/observable_parameter_model.ts
+++ plugins/block-shareable-procedures/src/observable_parameter_model.ts
@@ -33,7 +33,7 @@ export class ObservableParameterModel
     varId?: string,
   ) {
     this.id = id ?? Blockly.utils.idGenerator.genUid();
-   
+    this.createBackingVariable(name, varId);
   }
 
   /**
