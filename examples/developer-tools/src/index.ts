b8e9d50269512cb441f6d12c7fb8e78479c1f673
Maribeth Moffatt <maribethb@google.com>
2024-01-31 13:03:03 -0800

    feat: add basic saving and loading and start block state

--- examples/developer-tools/src/index.ts
+++ examples/developer-tools/src/index.ts
@@ -7,6 +7,7 @@
 import * as Blockly from 'blockly/core';
 import * as En from 'blockly/msg/en';
 import {registerAllBlocks} from './blocks';
+import {save, load} from './serialization';
 import {toolbox} from './toolbox';
 import {theme} from './theme';
 import 'blockly/blocks';
@@ -28,3 +29,26 @@ const definitionDiv = document.getElementById('block-definition').firstChild;
 
 const previewWorkspace = Blockly.inject(previewDiv, {});
 const mainWorkspace = Blockly.inject(mainWorkspaceDiv, {theme, toolbox});
+
+// Disable orphan blocks on the main workspace
+mainWorkspace.addChangeListener(Blockly.Events.disableOrphans);
+
+// Load either saved state or new state
+load(mainWorkspace);
+
+// Whenever the workspace changes meaningfully, update the preview.
+mainWorkspace.addChangeListener((e: Blockly.Events.Abstract) => {
+  // Don't run the code when the workspace finishes loading; we're
+  // already running it once when the application starts.
+  // Don't run the code during drags; we might have invalid state.
+  if (
+    e.isUiEvent ||
+    e.type == Blockly.Events.FINISHED_LOADING ||
+    mainWorkspace.isDragging()
+  ) {
+    return;
+  }
+
+  save(mainWorkspace);
+  previewWorkspace.clear();
+});
