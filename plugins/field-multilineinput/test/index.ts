6e2274d3dcb2a1db0ece64f4de500d41f6507c33
Rachel Fenichel <fenichel@google.com>
2024-02-23 11:40:55 -0800

    feat!: add block definition to multiline text field plugin (#2202)
    * chore: force-install blockly 10.4.0-beta.1 for development
    
    * feat: add text_multiline block and associated generators
    
    * feat: update test page to use new block and field
    
    * feat: README
    
    * chore: respond to PR feedback
    
    BREAKING CHANGE: The multiline text input field no longer registers itself on load. The developer must either manually register the field or install blocks, which will install the field. This is part of a move to have no side effects in field and block definitions, so that tree-shaking can remove unwanted fields and blocks.

--- plugins/field-multilineinput/test/index.ts
+++ plugins/field-multilineinput/test/index.ts
@@ -9,30 +9,200 @@
  */
 
 import * as Blockly from 'blockly';
-import {generateFieldTestBlocks, createPlayground} from '@blockly/dev-tools';
-import '../src/index';
+import {javascriptGenerator} from 'blockly/javascript';
+import {dartGenerator} from 'blockly/dart';
+import {phpGenerator} from 'blockly/php';
+import {pythonGenerator} from 'blockly/python';
+import {luaGenerator} from 'blockly/lua';
+import {createPlayground} from '@blockly/dev-tools';
+import {textMultiline} from '../src/index';
 
-const toolbox = generateFieldTestBlocks('field_multilinetext', [
+/**
+ * An array of blocks that are defined only for the purposes of
+ * manually and visually testing the multiline text input field.
+ */
+const testBlockDefinitions = [
   {
-    label: 'Spellchecked',
-    args: {
-      spellcheck: true,
-      text: `’Twas brillig, and the slithy toves
+    type: 'test_spellchecked_field',
+    message0: '%1',
+    args0: [
+      {
+        type: 'field_multilinetext',
+        name: 'FIELDNAME',
+        spellcheck: true,
+        text: `’Twas brillig, and the slithy toves
   Did gyre and gimble in the wabe:
 All mimsy were the borogoves,
   And the mome raths outgrabe.`,
-    },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
   {
-    label: 'Not spel chekt',
-    args: {
-      spellcheck: false,
-      text:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed' +
-        ' do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
+    type: 'test_unspellchecked_field',
+    message0: '%1',
+    args0: [
+      {
+        type: 'field_multilinetext',
+        name: 'FIELDNAME',
+        spellcheck: false,
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
-]);
+  {
+    type: 'test_spellchecked_field_and_label',
+    message0: 'block %1',
+    args0: [
+      {
+        type: 'field_multilinetext',
+        name: 'FIELDNAME',
+        spellcheck: true,
+        text: `’Twas brillig, and the slithy toves
+  Did gyre and gimble in the wabe:
+All mimsy were the borogoves,
+  And the mome raths outgrabe.`,
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
+  },
+  {
+    type: 'test_unspellchecked_field_and_label',
+    message0: 'block %1',
+    args0: [
+      {
+        type: 'field_multilinetext',
+        name: 'FIELDNAME',
+        spellcheck: false,
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
+  },
+  {
+    type: 'test_parent_block',
+    message0: 'parent %1',
+    args0: [
+      {
+        type: 'input_value',
+        name: 'INPUT',
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    style: 'loop_blocks',
+  },
+];
+
+Blockly.defineBlocksWithJsonArray(testBlockDefinitions);
+
+/**
+ * A test toolbox containing the exported blocks and a variety of
+ * test blocks to exercise the multiline text input field in different
+ * contexts (on a shadow block, as the only field on a block, etc).
+ * These are in a simple toolbox, rather than a category toolbox, so that
+ * they are all instantiated every time the test page is opened.
+ */
+const jsonToolbox = {
+  contents: [
+    {
+      kind: 'label',
+      text: 'Exported blocks',
+    },
+    {
+      kind: 'block',
+      type: 'text_multiline',
+    },
+    {
+      kind: 'label',
+      text: 'Test blocks: spellchecked field',
+    },
+    {
+      kind: 'block',
+      type: 'test_spellchecked_field',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_spellchecked_field',
+          },
+        },
+      },
+    },
+    {
+      kind: 'block',
+      type: 'test_spellchecked_field_and_label',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_spellchecked_field_and_label',
+          },
+        },
+      },
+    },
+    {
+      kind: 'label',
+      text: 'Test blocks: unspellchecked field',
+    },
+    {
+      kind: 'block',
+      type: 'test_unspellchecked_field',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_unspellchecked_field',
+          },
+        },
+      },
+    },
+    {
+      kind: 'block',
+      type: 'test_unspellchecked_field_and_label',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_unspellchecked_field_and_label',
+          },
+        },
+      },
+    },
+  ],
+};
+
+/**
+ * Uninstall the base multiline text block and its associated generators.
+ * TODO(#2194): remove this when those blocks are removed from the core library.
+ */
+function uninstallBlock() {
+  delete Blockly.Blocks['text_multiline'];
+  delete javascriptGenerator.forBlock['text_multiline'];
+  delete dartGenerator.forBlock['text_multiline'];
+  delete luaGenerator.forBlock['text_multiline'];
+  delete pythonGenerator.forBlock['text_multiline'];
+  delete phpGenerator.forBlock['text_multiline'];
+}
 
 /**
  * Create a workspace.
@@ -45,13 +215,21 @@ function createWorkspace(
   blocklyDiv: HTMLElement,
   options: Blockly.BlocklyOptions,
 ): Blockly.WorkspaceSvg {
+  uninstallBlock();
+  textMultiline.installBlock({
+    javascript: javascriptGenerator,
+    dart: dartGenerator,
+    lua: luaGenerator,
+    python: pythonGenerator,
+    php: phpGenerator,
+  });
   const workspace = Blockly.inject(blocklyDiv, options);
   return workspace;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   const defaultOptions: Blockly.BlocklyOptions = {
-    toolbox,
+    toolbox: jsonToolbox,
   };
   const rootElement = document.getElementById('root');
   if (rootElement) {
