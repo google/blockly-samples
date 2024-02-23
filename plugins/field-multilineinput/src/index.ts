fff0cb78948c0b117327fcdd5c9dad045738801d
Rachel Fenichel <fenichel@google.com>
2024-02-23 11:40:55 -0800

    feat!: add block definition to multiline text field plugin (#2202)
    * chore: force-install blockly 10.4.0-beta.1 for development
    
    * feat: add text_multiline block and associated generators
    
    * feat: update test page to use new block and field
    
    * feat: README
    
    * chore: respond to PR feedback
    
    BREAKING CHANGE: The multiline text input field no longer registers itself on load. The developer must either manually register the field or install blocks, which will install the field. This is part of a move to have no side effects in field and block definitions, so that tree-shaking can remove unwanted fields and blocks.

--- plugins/field-multilineinput/src/index.ts
+++ plugins/field-multilineinput/src/index.ts
@@ -4,4 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as textMultiline from './blocks/textMultiline';
+
 export * from './field_multilineinput';
+
+// Re-export all parts of the block definition.
+export * as textMultiline from './blocks/textMultiline';
+
+// This package currently exports a single block. More may
+// be added later.
+export const installAllBlocks = textMultiline.installBlock;
