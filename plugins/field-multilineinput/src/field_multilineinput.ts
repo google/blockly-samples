c29c02285e9f0db5666c247212124cab0828db73
Rachel Fenichel <fenichel@google.com>
2024-02-23 11:40:55 -0800

    feat!: add block definition to multiline text field plugin (#2202)
    * chore: force-install blockly 10.4.0-beta.1 for development
    
    * feat: add text_multiline block and associated generators
    
    * feat: update test page to use new block and field
    
    * feat: README
    
    * chore: respond to PR feedback
    
    BREAKING CHANGE: The multiline text input field no longer registers itself on load. The developer must either manually register the field or install blocks, which will install the field. This is part of a move to have no side effects in field and block definitions, so that tree-shaking can remove unwanted fields and blocks.

--- plugins/field-multilineinput/src/field_multilineinput.ts
+++ plugins/field-multilineinput/src/field_multilineinput.ts
@@ -491,13 +491,19 @@ export class FieldMultilineInput extends Blockly.FieldTextInput {
   }
 }
 
-// Unregister legacy field_multilinetext that was in core.  Delete this once
-// core Blockly no longer defines field_multilinetext.
-// If field_multilinetext is not defined in core,
-// this generates a console warning.
-Blockly.fieldRegistry.unregister('field_multilinetext');
+/**
+ * Register the field and any dependencies.
+ */
+export function registerFieldMultilineInput() {
+  // Unregister legacy field_multilinetext that was in core.
+  // TODO(2194): Delete this once core Blockly no longer defines
+  // field_multilinetext.
+  // If field_multilinetext is not defined in core,
+  // this generates a console warning.
+  Blockly.fieldRegistry.unregister('field_multilinetext');
 
-Blockly.fieldRegistry.register('field_multilinetext', FieldMultilineInput);
+  Blockly.fieldRegistry.register('field_multilinetext', FieldMultilineInput);
+}
 
 /**
  * CSS for multiline field.
