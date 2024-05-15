9959cb402ef165f26e6664e69732b67279079060
Rachel Fenichel <fenichel@google.com>
2024-05-15 15:17:32 -0700

    fix: stop using generateFieldTestBlocks for dropdown grid field

--- plugins/field-grid-dropdown/test/index.ts
+++ plugins/field-grid-dropdown/test/index.ts
@@ -9,219 +9,443 @@
  */
 
 import * as Blockly from 'blockly';
-import {generateFieldTestBlocks, createPlayground} from '@blockly/dev-tools';
+import {createPlayground} from '@blockly/dev-tools';
 import '../src/index';
 
-const toolbox = generateFieldTestBlocks('field_grid_dropdown', [
+/**
+ * An array of blocks that are defined only for the purposes of
+ * manually and visually testing the dropdown grid field.
+ */
+const testBlockDefinitions = [
   {
-    label: 'Different text length',
-    args: {
-      options: [
-        ['A', 'A'],
-        ['long text ', 'long text'],
-        ['B', 'B'],
-        ['C', 'C'],
-        [
-          'really really really loooooong text',
-          'really really really loooooong text',
+    type: 'test_standard_field_values',
+    message0: '%1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        options: [
+          ['A', 'A'],
         ],
-        ['D', 'D'],
-        ['E', 'E'],
-      ],
-    },
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
   {
-    label: 'Long text list',
-    args: {
-      options: [
-        ['A', 'A'],
-        ['B', 'B'],
-        ['C', 'C'],
-        ['D', 'D'],
-        ['E', 'E'],
-        ['D', 'F'],
-        ['G', 'G'],
-        ['H', 'H'],
-        ['I', 'I'],
-        ['J', 'J'],
-        ['K', 'K'],
-        ['L', 'L'],
-        ['M', 'M'],
-        ['N', 'N'],
-        ['O', 'O'],
-        ['P', 'P'],
-        ['Q', 'Q'],
-        ['R', 'R'],
-        ['S', 'S'],
-        ['T', 'T'],
-        ['U', 'U'],
-        ['V', 'V'],
-        ['W', 'W'],
-        ['X', 'X'],
-        ['Y', 'Y'],
-        ['Z', 'Z'],
-      ],
-    },
+    type: 'test_custom_field_values',
+    message0: 'Different text length %1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        options: [
+          ['A', 'A'],
+          ['long text ', 'long text'],
+          ['B', 'B'],
+          ['C', 'C'],
+          [
+            'really really really loooooong text',
+            'really really really loooooong text',
+          ],
+          ['D', 'D'],
+          ['E', 'E'],
+        ],
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
+  },  {
+    type: 'test_custom_field_values_long_text_list',
+    message0: 'Long text list %1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        options: [
+          ['A', 'A'],
+          ['B', 'B'],
+          ['C', 'C'],
+          ['D', 'D'],
+          ['E', 'E'],
+          ['D', 'F'],
+          ['G', 'G'],
+          ['H', 'H'],
+          ['I', 'I'],
+          ['J', 'J'],
+          ['K', 'K'],
+          ['L', 'L'],
+          ['M', 'M'],
+          ['N', 'N'],
+          ['O', 'O'],
+          ['P', 'P'],
+          ['Q', 'Q'],
+          ['R', 'R'],
+          ['S', 'S'],
+          ['T', 'T'],
+          ['U', 'U'],
+          ['V', 'V'],
+          ['W', 'W'],
+          ['X', 'X'],
+          ['Y', 'Y'],
+          ['Z', 'Z'],
+        ],
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
+  },  {
+    type: 'test_custom_field_values_images',
+    message0: 'Images %1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        options: [
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/a.png',
+              width: 32,
+              height: 32,
+              alt: 'A',
+            },
+            'A',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/b.png',
+              width: 32,
+              height: 32,
+              alt: 'B',
+            },
+            'B',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/c.png',
+              width: 32,
+              height: 32,
+              alt: 'C',
+            },
+            'C',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/d.png',
+              width: 32,
+              height: 32,
+              alt: 'D',
+            },
+            'D',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/e.png',
+              width: 32,
+              height: 32,
+              alt: 'E',
+            },
+            'E',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/f.png',
+              width: 32,
+              height: 32,
+              alt: 'F',
+            },
+            'F',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/g.png',
+              width: 32,
+              height: 32,
+              alt: 'G',
+            },
+            'G',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/h.png',
+              width: 32,
+              height: 32,
+              alt: 'H',
+            },
+            'H',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/i.png',
+              width: 32,
+              height: 32,
+              alt: 'I',
+            },
+            'I',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/j.png',
+              width: 32,
+              height: 32,
+              alt: 'J',
+            },
+            'J',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/k.png',
+              width: 32,
+              height: 32,
+              alt: 'K',
+            },
+            'K',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/l.png',
+              width: 32,
+              height: 32,
+              alt: 'L',
+            },
+            'L',
+          ],
+          [
+            {
+              src: 'https://blockly-demo.appspot.com/static/tests/media/m.png',
+              width: 32,
+              height: 32,
+              alt: 'M',
+            },
+            'M',
+          ],
+        ],
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
   {
-    label: 'Images',
-    args: {
-      options: [
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/a.png',
-            width: 32,
-            height: 32,
-            alt: 'A',
-          },
-          'A',
+    type: 'test_custom_field_values_4_columns',
+    message0: '4 columns %1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        columns: '4',
+        options: [
+          ['A', 'A'],
+          ['B', 'B'],
+          ['C', 'C'],
+          ['D', 'D'],
+          ['E', 'E'],
+          ['F', 'F'],
+          ['G', 'G'],
+          ['H', 'H'],
         ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/b.png',
-            width: 32,
-            height: 32,
-            alt: 'B',
-          },
-          'B',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/c.png',
-            width: 32,
-            height: 32,
-            alt: 'C',
-          },
-          'C',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/d.png',
-            width: 32,
-            height: 32,
-            alt: 'D',
-          },
-          'D',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/e.png',
-            width: 32,
-            height: 32,
-            alt: 'E',
-          },
-          'E',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/f.png',
-            width: 32,
-            height: 32,
-            alt: 'F',
-          },
-          'F',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/g.png',
-            width: 32,
-            height: 32,
-            alt: 'G',
-          },
-          'G',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/h.png',
-            width: 32,
-            height: 32,
-            alt: 'H',
-          },
-          'H',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/i.png',
-            width: 32,
-            height: 32,
-            alt: 'I',
-          },
-          'I',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/j.png',
-            width: 32,
-            height: 32,
-            alt: 'J',
-          },
-          'J',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/k.png',
-            width: 32,
-            height: 32,
-            alt: 'K',
-          },
-          'K',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/l.png',
-            width: 32,
-            height: 32,
-            alt: 'L',
-          },
-          'L',
-        ],
-        [
-          {
-            src: 'https://blockly-demo.appspot.com/static/tests/media/m.png',
-            width: 32,
-            height: 32,
-            alt: 'M',
-          },
-          'M',
-        ],
-      ],
-    },
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
   {
-    label: '4 columns',
-    args: {
-      columns: '4',
-      options: [
-        ['A', 'A'],
-        ['B', 'B'],
-        ['C', 'C'],
-        ['D', 'D'],
-        ['E', 'E'],
-        ['F', 'F'],
-        ['G', 'G'],
-        ['H', 'H'],
-      ],
-    },
+    type: 'test_custom_field_values_custom_colours',
+    message0: 'Custom colours %1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        primaryColour: '#783105',
+        borderColour: '#d6a587',
+        options: [
+          ['A', 'A'],
+          ['B', 'B'],
+          ['C', 'C'],
+          ['D', 'D'],
+          ['E', 'E'],
+          ['F', 'F'],
+          ['G', 'G'],
+          ['H', 'H'],
+        ],
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
   {
-    label: 'custom colours',
-    args: {
-      primaryColour: '#783105',
-      borderColour: '#d6a587',
-      options: [
-        ['A', 'A'],
-        ['B', 'B'],
-        ['C', 'C'],
-        ['D', 'D'],
-        ['E', 'E'],
-        ['F', 'F'],
-        ['G', 'G'],
-        ['H', 'H'],
-      ],
-    },
+    type: 'test_standard_field_values_and_label',
+    message0: 'block %1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        options: [
+          ['A', 'A'],
+        ],
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
-]);
+  {
+    type: 'test_custom_field_values_and_label',
+    message0: 'block %1',
+    args0: [
+      {
+        type: 'field_grid_dropdown',
+        name: 'FIELDNAME',
+        options: [
+          ['A', 'A'],
+        ],
+        alt: {
+          type: 'field_label',
+          text: `No field_grid_dropdown`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
+  },
+  {
+    type: 'test_parent_block',
+    message0: 'parent %1',
+    args0: [
+      {
+        type: 'input_value',
+        name: 'INPUT',
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    style: 'loop_blocks',
+  },
+];
 
+Blockly.defineBlocksWithJsonArray(testBlockDefinitions);
+
+/**
+ * A test toolbox containing the exported blocks and a variety of
+ * test blocks to exercise the dropdown grid field in different contexts
+ * (on a shadow block, as the only field on a block, etc).
+ * These are in a simple toolbox, rather than a category toolbox, so that
+ * they are all instantiated every time the test page is opened.
+ */
+const jsonToolbox = {
+  contents: [
+    {
+      kind: 'label',
+      text: 'Test blocks: default field values',
+    },
+    {
+      kind: 'block',
+      type: 'test_standard_field_values',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_standard_field_values',
+          },
+        },
+      },
+    },
+    {
+      kind: 'block',
+      type: 'test_standard_field_values_and_label',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_standard_field_values_and_label',
+          },
+        },
+      },
+    },
+    {
+      kind: 'label',
+      text: 'Test blocks: custom field values',
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values',
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values_long_text_list',
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values_images',
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values_4_columns',
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values_custom_colours',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_custom_field_values',
+          },
+        },
+      },
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values_and_label',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_custom_field_values_and_label',
+          },
+        },
+      },
+    },
+  ],
+};
 /**
  * Create a workspace.
  *
@@ -239,7 +463,7 @@ function createWorkspace(
 
 document.addEventListener('DOMContentLoaded', function () {
   const defaultOptions: Blockly.BlocklyOptions = {
-    toolbox,
+    toolbox: jsonToolbox,
   };
   const rootElement = document.getElementById('root');
   if (rootElement) {
