6e1b37f4f2969dd533c5bdd339482d3844f1aeeb
Rachel Fenichel <fenichel@google.com>
2024-05-15 13:27:44 -0700

    fix: stop using generateFieldTestBlocks for bitmap field

--- plugins/field-bitmap/test/index.js
+++ plugins/field-bitmap/test/index.js
@@ -10,33 +10,188 @@
 
 import '../src/field-bitmap';
 
-import {createPlayground, generateFieldTestBlocks} from '@blockly/dev-tools';
+import {createPlayground} from '@blockly/dev-tools';
 import * as Blockly from 'blockly';
 
-const toolbox = generateFieldTestBlocks('field_bitmap', [
+/**
+ * An array of blocks that are defined only for the purposes of
+ * manually and visually testing the bitmap field.
+ */
+const testBlockDefinitions = [
   {
-    label: 'With Default',
-    args: {
-      value: [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 0, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0],
-        [0, 1, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-      ],
-    },
+    type: 'test_standard_field_values',
+    message0: '%1',
+    args0: [
+      {
+        type: 'field_bitmap',
+        name: 'FIELDNAME',
+        alt: {
+          type: 'field_label',
+          text: `No field_bitmap`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
   {
-    label: 'Empty',
-    args: {
-      width: 3,
-      height: 3,
-      value: undefined,
-    },
+    type: 'test_custom_field_values',
+    message0: '%1',
+    args0: [
+      {
+        type: 'field_bitmap',
+        name: 'FIELDNAME',
+        value: [
+          [1, 0, 1],
+          [0, 1, 0],
+          [0, 0, 0],
+        ],
+        width: 3,
+        height: 3,
+        alt: {
+          type: 'field_label',
+          text: `No field_bitmap`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
   },
-]);
+  {
+    type: 'test_standard_field_values_and_label',
+    message0: 'block %1',
+    args0: [
+      {
+        type: 'field_bitmap',
+        name: 'FIELDNAME',
+        alt: {
+          type: 'field_label',
+          text: `No field_bitmap`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
+  },
+  {
+    type: 'test_custom_field_values_and_label',
+    message0: 'block %1',
+    args0: [
+      {
+        type: 'field_bitmap',
+        name: 'FIELDNAME',
+        value: [
+          [1, 0, 1],
+          [0, 1, 0],
+          [0, 0, 0],
+        ],
+        width: 3,
+        height: 3,
+        alt: {
+          type: 'field_label',
+          text: `No field_bitmap`,
+        },
+      },
+    ],
+    output: null,
+    style: 'math_blocks',
+  },
+  {
+    type: 'test_parent_block',
+    message0: 'parent %1',
+    args0: [
+      {
+        type: 'input_value',
+        name: 'INPUT',
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    style: 'loop_blocks',
+  },
+];
+
+Blockly.defineBlocksWithJsonArray(testBlockDefinitions);
+
+/**
+ * A test toolbox containing the exported blocks and a variety of
+ * test blocks to exercise the bitmap field in different contexts
+ * (on a shadow block, as the only field on a block, etc).
+ * These are in a simple toolbox, rather than a category toolbox, so that
+ * they are all instantiated every time the test page is opened.
+ */
+const jsonToolbox = {
+  contents: [
+    {
+      kind: 'label',
+      text: 'Test blocks: default field values',
+    },
+    {
+      kind: 'block',
+      type: 'test_standard_field_values',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_standard_field_values',
+          },
+        },
+      },
+    },
+    {
+      kind: 'block',
+      type: 'test_standard_field_values_and_label',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_standard_field_values_and_label',
+          },
+        },
+      },
+    },
+    {
+      kind: 'label',
+      text: 'Test blocks: custom field values',
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_custom_field_values',
+          },
+        },
+      },
+    },
+    {
+      kind: 'block',
+      type: 'test_custom_field_values_and_label',
+    },
+    {
+      kind: 'block',
+      type: 'test_parent_block',
+      inputs: {
+        INPUT: {
+          shadow: {
+            type: 'test_custom_field_values_and_label',
+          },
+        },
+      },
+    },
+  ],
+};
 
 /**
  * Create a workspace.
@@ -51,7 +206,7 @@ function createWorkspace(blocklyDiv, options) {
 
 document.addEventListener('DOMContentLoaded', function () {
   const defaultOptions = {
-    toolbox,
+    toolbox: jsonToolbox,
   };
   createPlayground(
     document.getElementById('root'),
