f442ae11642c6a0eff797572ad043f0c1ae46107
Rachel Fenichel <fenichel@google.com>
2024-05-16 15:37:43 -0700

    chore: tidy import

--- examples/blockly-vue3/src/components/BlocklyComponent.vue
+++ examples/blockly-vue3/src/components/BlocklyComponent.vue
@@ -12,8 +12,8 @@
 
 import {onMounted, ref, shallowRef} from 'vue';
 import * as Blockly from 'blockly/core';
-import * as libraryBlocks from 'blockly/blocks';
 import * as En from 'blockly/msg/en';
+import 'blockly/blocks';
 
 const props = defineProps(['options']);
 const blocklyToolbox = ref();
