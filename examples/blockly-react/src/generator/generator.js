bc05ce3fccf96e6da2773fe52beda09ccfa64369
saigenix <sainathmahindrakar@gmail.com>
2024-07-01 19:10:47 +0530

    fix(examples): remove date field in react demo

--- examples/blockly-react/src/generator/generator.js
+++ examples/blockly-react/src/generator/generator.js
@@ -29,7 +29,3 @@ import {javascriptGenerator} from 'blockly/javascript';
 javascriptGenerator.forBlock['test_react_field'] = function (block) {
   return "console.log('custom block');\n";
 };
-
-javascriptGenerator.forBlock['test_react_date_field'] = function (block) {
-  return 'console.log(' + block.getField('DATE').getText() + ');\n';
-};
