70cacf18a0e16bca09a37b8fabfd0035bad053f3
Ronald Bourret <rbourret@google.com>
2025-10-22 20:47:50 +0000

    docs: Update context menu codelab for v12

--- examples/context-menu-codelab/starter-code/index.js
+++ examples/context-menu-codelab/starter-code/index.js
@@ -3,6 +3,7 @@
 let workspace = null;
 
 function start() {
+  Blockly.ContextMenuItems.registerCommentOptions();
   // Create main workspace.
   workspace = Blockly.inject('blocklyDiv', {
     toolbox: toolboxSimple,
